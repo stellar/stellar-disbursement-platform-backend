50d2111f07fc2d17f6d3f0ae81d0344558d4b0dc
Marcelo Salloum <marcelo@stellar.org>
2023-08-23 14:21:38 -0700

    Implemented the basics of AnchorPlatformAuthMonitoringJob.

--- internal/monitor/metric_tags.go
+++ internal/monitor/metric_tags.go
@@ -6,7 +6,11 @@ const (
 	SuccessfulQueryDurationTag MetricTag = "successful_queries_duration"
 	FailureQueryDurationTag    MetricTag = "failure_queries_duration"
 	HttpRequestDurationTag     MetricTag = "requests_duration_seconds"
-	DisbursementsCounterTag    MetricTag = "disbursements_counter"
+	// Disbursements:
+	DisbursementsCounterTag MetricTag = "disbursements_counter"
+	// AnchorPlatformAuthProtection
+	AnchorPlatformAuthProtectionEnsuredCounterTag MetricTag = "anchor_platform_auth_protection_ensured_counter"
+	AnchorPlatformAuthProtectionMissingCounterTag MetricTag = "anchor_platform_auth_protection_missing_counter"
 )
 
 func (m MetricTag) ListAll() []MetricTag {
