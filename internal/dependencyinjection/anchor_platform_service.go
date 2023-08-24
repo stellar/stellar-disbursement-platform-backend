db1a5712ec1afc8cf0d7a63ee9fa3a15029753bd
Marcelo Salloum <marcelo@stellar.org>
2023-08-24 16:53:25 -0700

    Add tests for NewAnchorPlatformAPIService.

--- internal/dependencyinjection/anchor_platform_service.go
+++ internal/dependencyinjection/anchor_platform_service.go
@@ -11,22 +11,28 @@ import (
 const anchorPlatformAPIServiceInstanceName = "anchor_platform_api_service_instance"
 
 func NewAnchorPlatformAPIService(anchorPlatformBasePlatformURL, anchorPlatformOutgoingJWTSecret string) (anchorplatform.AnchorPlatformAPIServiceInterface, error) {
-	// TODO: validate if anchorPlatformBasePlatformURL and anchorPlatformOutgoingJWTSecret are not empty
 	if anchorPlatformBasePlatformURL == "" {
 		return nil, fmt.Errorf("anchor platform base platform url cannot be empty")
 	}
-
 	if anchorPlatformOutgoingJWTSecret == "" {
 		return nil, fmt.Errorf("anchor platform outgoing JWT secret cannot be empty")
 	}
 
-	log.Infof("⚙️ Setting default anchor platform APO Service")
+	// If there is already an instance of the service, we return the same instance
+	if instance, ok := dependenciesStoreMap[anchorPlatformAPIServiceInstanceName]; ok {
+		if anchorPlatformAPIService, ok := instance.(anchorplatform.AnchorPlatformAPIServiceInterface); ok {
+			return anchorPlatformAPIService, nil
+		}
+		return nil, fmt.Errorf("trying to cast pre-existing SMS client for depencency injection")
+	}
+
 	// Setup Anchor Platform API Service
+	log.Infof("⚙️ Setting default anchor platform API Service")
 	anchorPlatformAPIService, err := anchorplatform.NewAnchorPlatformAPIService(httpclient.DefaultClient(), anchorPlatformBasePlatformURL, anchorPlatformOutgoingJWTSecret)
 	if err != nil {
 		return nil, fmt.Errorf("creating Anchor Platform API service: %w", err)
 	}
-
 	setInstance(anchorPlatformAPIServiceInstanceName, anchorPlatformAPIService)
+
 	return anchorPlatformAPIService, nil
 }
