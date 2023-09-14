12292653656a50710364446c51a5a4c41621822d
Marwen Abid <marwen.abid@stellar.org>
2023-09-14 13:05:49 -0400

    Change Sep10 domain for Vibrant to api-dev instead of api

--- internal/services/wallets/wallets_testnet.go
+++ internal/services/wallets/wallets_testnet.go
@@ -23,7 +23,7 @@ var TestnetWallets = []data.Wallet{
 		Name:              "Vibrant Assist",
 		Homepage:          "https://vibrantapp.com/vibrant-assist",
 		DeepLinkSchema:    "https://vibrantapp.com/sdp-dev",
-		SEP10ClientDomain: "api.vibrantapp.com",
+		SEP10ClientDomain: "api-dev.vibrantapp.com",
 		Assets: []data.Asset{
 			{
 				Code:   "USDC",
