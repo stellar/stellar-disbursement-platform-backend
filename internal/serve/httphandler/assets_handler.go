0aabb19139bd9c0699a53e6a6b6319ec624f9383
Marcelo Salloum dos Santos <marcelo@stellar.org>
2023-10-02 15:06:38 -0700

    SDP-884: solve intermittent test failure due to timestamp comparison (#59)
    ### What
    
    Add an option to specify a custom `GetPreconditionsFn` parameter in the AssetsHandler object.
    
    ### Why
    
    To address the intermittent test failure ([example](https://github.com/stellar/stellar-disbursement-platform-backend/actions/runs/6200706140/job/16835930483?pr=40)) where the test assertion would fail because the time.Now() call generated in the test would differ from the one generated in the code by a second.

--- internal/serve/httphandler/assets_handler.go
+++ internal/serve/httphandler/assets_handler.go
@@ -34,9 +34,10 @@ const (
 var errCouldNotRemoveTrustline = errors.New("could not remove trustline")
 
 type AssetsHandler struct {
-	Models           *data.Models
-	HorizonClient    horizonclient.ClientInterface
-	SignatureService engine.SignatureService
+	Models             *data.Models
+	HorizonClient      horizonclient.ClientInterface
+	SignatureService   engine.SignatureService
+	GetPreconditionsFn func() txnbuild.Preconditions
 }
 
 type AssetRequest struct {
@@ -269,6 +270,10 @@ func (c AssetsHandler) submitChangeTrustTransaction(ctx context.Context, acc *ho
 		operations = append(operations, ctOp)
 	}
 
+	preconditions := txnbuild.Preconditions{TimeBounds: txnbuild.NewTimeout(20)}
+	if c.GetPreconditionsFn != nil {
+		preconditions = c.GetPreconditionsFn()
+	}
 	tx, err := txnbuild.NewTransaction(
 		txnbuild.TransactionParams{
 			SourceAccount: &txnbuild.SimpleAccount{
@@ -278,7 +283,7 @@ func (c AssetsHandler) submitChangeTrustTransaction(ctx context.Context, acc *ho
 			IncrementSequenceNum: true,
 			Operations:           operations,
 			BaseFee:              txnbuild.MinBaseFee * feeMultiplierInStroops,
-			Preconditions:        txnbuild.Preconditions{TimeBounds: txnbuild.NewTimeout(20)},
+			Preconditions:        preconditions,
 		},
 	)
 	if err != nil {
