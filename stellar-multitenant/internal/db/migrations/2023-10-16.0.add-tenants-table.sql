7889cf71f53256d3f73be8486eba28bf6ea214ca
Caio Teixeira <caio.xd63@hotmail.com>
2023-10-23 16:38:45 -0300

    feat: add config-tenant command

--- stellar-multitenant/internal/db/migrations/2023-10-16.0.add-tenants-table.sql
+++ stellar-multitenant/internal/db/migrations/2023-10-16.0.add-tenants-table.sql
@@ -18,21 +18,25 @@ CREATE TYPE public.sms_sender_type AS ENUM ('TWILIO_SMS', 'AWS_SMS', 'DRY_RUN');
 CREATE TABLE public.tenants
 (
     id VARCHAR(36) PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name text NULL,
-    email_sender_type email_sender_type NULL,
-    sms_sender_type sms_sender_type   NULL,
+    name text NOT NULL,
+    email_sender_type email_sender_type DEFAULT 'DRY_RUN'::email_sender_type,
+    sms_sender_type sms_sender_type DEFAULT 'DRY_RUN'::sms_sender_type,
     sep10_signing_public_key text NULL,
     distribution_public_key text NULL,
-    enable_mfa boolean NULL,
-    enable_recaptcha boolean NULL,
-    cors_allowed_origins text NULL,
+    enable_mfa boolean DEFAULT true,
+    enable_recaptcha boolean DEFAULT true,
+    cors_allowed_origins text[] NULL,
     base_url text NULL,
+    sdp_ui_base_url text NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 CREATE UNIQUE INDEX idx_unique_name ON public.tenants (LOWER(name));
 CREATE TRIGGER refresh_tenants_updated_at BEFORE UPDATE ON public.tenants FOR EACH ROW EXECUTE PROCEDURE update_at_refresh();
+
+COMMENT ON COLUMN tenants.base_url IS 'The SDP backend server''s base URL';
+COMMENT ON COLUMN tenants.sdp_ui_base_url IS 'The SDP UI/dashboard Base URL.';
 
 -- +migrate Down
 
--- stellar-multitenant/pkg/tenant/manager.go
+++ stellar-multitenant/pkg/tenant/manager.go
@@ -2,14 +2,17 @@ package tenant
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/lib/pq"
 	"github.com/stellar/stellar-disbursement-platform-backend/stellar-multitenant/internal/db"
 )
 
 var (
+	ErrTenantDoesNotExist   = errors.New("tenant does not exist")
 	ErrDuplicatedTenantName = errors.New("duplicated tenant name")
 	ErrEmptyTenantName      = errors.New("tenant name cannot be empty")
 )
@@ -23,7 +26,7 @@ func (m *Manager) AddTenant(ctx context.Context, name string) (*Tenant, error) {
 		return nil, ErrEmptyTenantName
 	}
 
-	const q = "INSERT INTO tenants (name) VALUES ($1) RETURNING id, name"
+	const q = "INSERT INTO tenants (name) VALUES ($1) RETURNING *"
 	var t Tenant
 	if err := m.db.GetContext(ctx, &t, q, name); err != nil {
 		if pqError, ok := err.(*pq.Error); ok && pqError.Constraint == "idx_unique_name" {
@@ -31,6 +34,86 @@ func (m *Manager) AddTenant(ctx context.Context, name string) (*Tenant, error) {
 		}
 		return nil, fmt.Errorf("inserting tenant %s: %w", name, err)
 	}
+	return &t, nil
+}
+
+func (m *Manager) UpdateTenantConfig(ctx context.Context, tu *TenantUpdate) (*Tenant, error) {
+	if tu == nil {
+		return nil, fmt.Errorf("tenant update cannot be nil")
+	}
+
+	if err := tu.Validate(); err != nil {
+		return nil, err
+	}
+
+	q := `
+		UPDATE tenants
+		SET
+			%s
+		WHERE
+			id = ?
+		RETURNING *
+	`
+
+	fields := make([]string, 0)
+	args := make([]interface{}, 0)
+	if tu.EmailSenderType != nil {
+		fields = append(fields, "email_sender_type = ?")
+		args = append(args, *tu.EmailSenderType)
+	}
+
+	if tu.SMSSenderType != nil {
+		fields = append(fields, "sms_sender_type = ?")
+		args = append(args, *tu.SMSSenderType)
+	}
+
+	if tu.SEP10SigningPublicKey != nil {
+		fields = append(fields, "sep10_signing_public_key = ?")
+		args = append(args, *tu.SEP10SigningPublicKey)
+	}
+
+	if tu.DistributionPublicKey != nil {
+		fields = append(fields, "distribution_public_key = ?")
+		args = append(args, *tu.DistributionPublicKey)
+	}
+
+	if tu.EnableMFA != nil {
+		fields = append(fields, "enable_mfa = ?")
+		args = append(args, *tu.EnableMFA)
+	}
+
+	if tu.EnableReCAPTCHA != nil {
+		fields = append(fields, "enable_recaptcha = ?")
+		args = append(args, *tu.EnableReCAPTCHA)
+	}
+
+	if tu.BaseURL != nil {
+		fields = append(fields, "base_url = ?")
+		args = append(args, *tu.BaseURL)
+	}
+
+	if tu.SDPUIBaseURL != nil {
+		fields = append(fields, "sdp_ui_base_url = ?")
+		args = append(args, *tu.SDPUIBaseURL)
+	}
+
+	if tu.CORSAllowedOrigins != nil && len(tu.CORSAllowedOrigins) > 0 {
+		fields = append(fields, "cors_allowed_origins = ?")
+		args = append(args, pq.Array(tu.CORSAllowedOrigins))
+	}
+
+	args = append(args, tu.ID)
+	q = fmt.Sprintf(q, strings.Join(fields, ",\n"))
+	q = m.db.Rebind(q)
+
+	var t Tenant
+	if err := m.db.GetContext(ctx, &t, q, args...); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrTenantDoesNotExist
+		}
+		return nil, fmt.Errorf("updating tenant ID %s: %w", tu.ID, err)
+	}
+
 	return &t, nil
 }
 
