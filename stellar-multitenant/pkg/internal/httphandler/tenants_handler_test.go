dfbcbc32b3feeb999011f9ea21f4147dab1ee958
Cecília Romão <ceciliaromao@gmail.com>
2023-11-14 20:07:00 -0300

    Update tenants_handler_test.go

--- stellar-multitenant/pkg/internal/httphandler/tenants_handler_test.go
+++ stellar-multitenant/pkg/internal/httphandler/tenants_handler_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_GetTenant(t *testing.T) {
+func Test_Get(t *testing.T) {
 	dbt := dbtest.OpenWithTenantMigrationsOnly(t)
 	defer dbt.Close()
 
@@ -30,15 +30,17 @@ func Test_GetTenant(t *testing.T) {
 		Manager: &tenant.Manager{},
 	}
 
-	tnt1 := tenant.CreateTenantFixture(t, ctx, dbConnectionPool, "myorg1")
-	tnt2 := tenant.CreateTenantFixture(t, ctx, dbConnectionPool, "myorg2")
+	t.Run("GetAll successfully returns a list of all tenants", func(t *testing.T) {
+		tenant.DeleteAllTenantFixtures(t, ctx, dbConnectionPool)
 
-	tnt1JSON, err := json.Marshal(tnt1)
-	require.NoError(t, err)
-	tnt2JSON, err := json.Marshal(tnt2)
-	require.NoError(t, err)
+		tnt1 := tenant.CreateTenantFixture(t, ctx, dbConnectionPool, "myorg1")
+		tnt2 := tenant.CreateTenantFixture(t, ctx, dbConnectionPool, "myorg2")
 
-	t.Run("successfully returns a list of all tenants", func(t *testing.T) {
+		tnt1JSON, err := json.Marshal(tnt1)
+		require.NoError(t, err)
+		tnt2JSON, err := json.Marshal(tnt2)
+		require.NoError(t, err)
+
 		expectedJSON := fmt.Sprintf("[%s, %s]", tnt1JSON, tnt2JSON)
 
 		rr := httptest.NewRecorder()
@@ -54,11 +56,18 @@ func Test_GetTenant(t *testing.T) {
 		assert.JSONEq(t, expectedJSON, string(respBody))
 	})
 
-	t.Run("successfully returns a tenant by ID", func(t *testing.T) {
+	t.Run("GetByID successfully returns a tenant by ID", func(t *testing.T) {
+		tenant.DeleteAllTenantFixtures(t, ctx, dbConnectionPool)
+
+		tnt1 := tenant.CreateTenantFixture(t, ctx, dbConnectionPool, "myorg1")
+		_ = tenant.CreateTenantFixture(t, ctx, dbConnectionPool, "myorg2")
+		tnt1JSON, err := json.Marshal(tnt1)
+		require.NoError(t, err)
+
 		url := fmt.Sprintf("/tenants/%s", tnt1.ID)
 		rr := httptest.NewRecorder()
 		req, _ := http.NewRequest("GET", url, nil)
-		http.HandlerFunc(handler.GetAll).ServeHTTP(rr, req)
+		http.HandlerFunc(handler.GetByID).ServeHTTP(rr, req)
 
 		resp := rr.Result()
 
@@ -69,11 +78,18 @@ func Test_GetTenant(t *testing.T) {
 		assert.JSONEq(t, string(tnt1JSON), string(respBody))
 	})
 
-	t.Run("successfully returns a tenant by name", func(t *testing.T) {
+	t.Run("GetByName successfully returns a tenant by name", func(t *testing.T) {
+		tenant.DeleteAllTenantFixtures(t, ctx, dbConnectionPool)
+
+		_ = tenant.CreateTenantFixture(t, ctx, dbConnectionPool, "myorg1")
+		tnt2 := tenant.CreateTenantFixture(t, ctx, dbConnectionPool, "myorg2")
+		tnt2JSON, err := json.Marshal(tnt2)
+		require.NoError(t, err)
+
 		url := fmt.Sprintf("/tenants/%s", tnt2.Name)
 		rr := httptest.NewRecorder()
 		req, _ := http.NewRequest("GET", url, nil)
-		http.HandlerFunc(handler.GetAll).ServeHTTP(rr, req)
+		http.HandlerFunc(handler.GetByName).ServeHTTP(rr, req)
 
 		resp := rr.Result()
 
