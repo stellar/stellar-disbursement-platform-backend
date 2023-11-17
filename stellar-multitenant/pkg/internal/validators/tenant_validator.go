de5d8eae651b63b8308720b980ec31c60ba37d3e
Caio Teixeira <caio.xd63@hotmail.com>
2023-11-17 11:19:36 -0300

    feat: add resquest validators

---
+++ stellar-multitenant/pkg/internal/validators/tenant_validator.go
@@ -0,0 +1,84 @@
+package validators
+
+import (
+	"fmt"
+	"net/url"
+	"regexp"
+
+	"github.com/stellar/go/strkey"
+	"github.com/stellar/stellar-disbursement-platform-backend/internal/utils"
+	"github.com/stellar/stellar-disbursement-platform-backend/stellar-multitenant/pkg/tenant"
+)
+
+var validTenantName *regexp.Regexp = regexp.MustCompile(`^[a-z-]+$`)
+
+type TenantRequest struct {
+	Name                  string                 `json:"name"`
+	OwnerEmail            string                 `json:"owner_email"`
+	OwnerFirstName        string                 `json:"owner_first_name"`
+	OwnerLastName         string                 `json:"owner_last_name"`
+	OrganizationName      string                 `json:"organization_name"`
+	NetworkType           string                 `json:"network_type"`
+	EmailSenderType       tenant.EmailSenderType `json:"email_sender_type"`
+	SMSSenderType         tenant.SMSSenderType   `json:"sms_sender_type"`
+	SEP10SigningPublicKey string                 `json:"sep10_signing_public_key"`
+	DistributionPublicKey string                 `json:"distribution_public_key"`
+	EnableMFA             bool                   `json:"enable_mfa"`
+	EnableReCAPTCHA       bool                   `json:"enable_recaptcha"`
+	CORSAllowedOrigins    []string               `json:"cors_allowed_origins"`
+	BaseURL               string                 `json:"base_url"`
+	SDPUIBaseURL          string                 `json:"sdp_ui_base_url"`
+}
+
+type TenantValidator struct {
+	*Validator
+}
+
+func NewTenantValidator() *TenantValidator {
+	return &TenantValidator{Validator: NewValidator()}
+}
+
+func (tv *TenantValidator) ValidateCreateTenantRequest(reqBody *TenantRequest) *TenantRequest {
+	tv.Check(reqBody != nil, "body", "request body is empty")
+	if tv.HasErrors() {
+		return nil
+	}
+
+	tv.Check(validTenantName.MatchString(reqBody.Name), "name", "invalid tenant name. It should only contains lower case letters and dash (-)")
+	tv.CheckError(utils.ValidateEmail(reqBody.OwnerEmail), "owner_email", "invalid email")
+	tv.Check(reqBody.OwnerFirstName != "", "owner_first_name", "owner_first_name is required")
+	tv.Check(reqBody.OwnerLastName != "", "owner_last_name", "owner_last_name is required")
+	tv.Check(reqBody.OrganizationName != "", "organization_name", "organization_name is required")
+	tv.Check(reqBody.NetworkType == "pubnet" || reqBody.NetworkType == "testnet", "network_type", "invalid network type provided. Expected one of these values: pubnet or testnet")
+
+	var err error
+	reqBody.EmailSenderType, err = tenant.ParseEmailSenderType(string(reqBody.EmailSenderType))
+	tv.CheckError(err, "email_sender_type", fmt.Sprintf("invalid email sender type. Expected one of these values: %s", []tenant.EmailSenderType{tenant.AWSEmailSenderType, tenant.DryRunEmailSenderType}))
+
+	reqBody.SMSSenderType, err = tenant.ParseSMSSenderType(string(reqBody.SMSSenderType))
+	tv.CheckError(err, "sms_sender_type", fmt.Sprintf("invalid sms sender type. Expected one of these values: %s", []tenant.SMSSenderType{tenant.TwilioSMSSenderType, tenant.AWSSMSSenderType, tenant.DryRunSMSSenderType}))
+
+	tv.Check(strkey.IsValidEd25519PublicKey(reqBody.SEP10SigningPublicKey), "sep10_signing_public_key", "invalid public key")
+	tv.Check(strkey.IsValidEd25519PublicKey(reqBody.DistributionPublicKey), "distribution_public_key", "invalid public key")
+
+	if _, err = url.ParseRequestURI(reqBody.BaseURL); err != nil {
+		tv.Check(false, "base_url", "invalid base URL value")
+	}
+
+	if _, err = url.ParseRequestURI(reqBody.SDPUIBaseURL); err != nil {
+		tv.Check(false, "sdp_ui_base_url", "invalid SDP UI base URL value")
+	}
+
+	tv.Check(len(reqBody.CORSAllowedOrigins) != 0, "cors_allowed_origins", "provide at least one CORS allowed origins")
+	for i, cors := range reqBody.CORSAllowedOrigins {
+		if _, err = url.ParseRequestURI(cors); err != nil {
+			tv.Check(false, "cors_allowed_origins", fmt.Sprintf("invalid URL value for cors_allowed_origins[%d]=%s", i, cors))
+		}
+	}
+
+	if tv.HasErrors() {
+		return nil
+	}
+
+	return reqBody
+}
---
+++ stellar-multitenant/pkg/internal/validators/tenant_validator_test.go
@@ -0,0 +1,278 @@
+package validators
+
+import (
+	"testing"
+
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/stellar-disbursement-platform-backend/stellar-multitenant/pkg/tenant"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTenantValidator_ValidateCreateTenantRequest(t *testing.T) {
+	t.Run("returns error when request body is empty", func(t *testing.T) {
+		tv := NewTenantValidator()
+		tv.ValidateCreateTenantRequest(nil)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{"body": "request body is empty"}, tv.Errors)
+	})
+
+	t.Run("returns error when request body has empty fields", func(t *testing.T) {
+		tv := NewTenantValidator()
+		reqBody := &TenantRequest{}
+
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{
+			"name":                     "invalid tenant name. It should only contains lower case letters and dash (-)",
+			"owner_email":              "invalid email",
+			"owner_first_name":         "owner_first_name is required",
+			"owner_last_name":          "owner_last_name is required",
+			"organization_name":        "organization_name is required",
+			"base_url":                 "invalid base URL value",
+			"email_sender_type":        "invalid email sender type. Expected one of these values: [AWS_EMAIL DRY_RUN]",
+			"sms_sender_type":          "invalid sms sender type. Expected one of these values: [TWILIO_SMS AWS_SMS DRY_RUN]",
+			"cors_allowed_origins":     "provide at least one CORS allowed origins",
+			"network_type":             "invalid network type provided. Expected one of these values: pubnet or testnet",
+			"sdp_ui_base_url":          "invalid SDP UI base URL value",
+			"sep10_signing_public_key": "invalid public key",
+			"distribution_public_key":  "invalid public key",
+		}, tv.Errors)
+
+		reqBody.Name = "aid-org"
+		tv.Errors = map[string]interface{}{}
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{
+			"owner_email":              "invalid email",
+			"owner_first_name":         "owner_first_name is required",
+			"owner_last_name":          "owner_last_name is required",
+			"organization_name":        "organization_name is required",
+			"base_url":                 "invalid base URL value",
+			"email_sender_type":        "invalid email sender type. Expected one of these values: [AWS_EMAIL DRY_RUN]",
+			"sms_sender_type":          "invalid sms sender type. Expected one of these values: [TWILIO_SMS AWS_SMS DRY_RUN]",
+			"cors_allowed_origins":     "provide at least one CORS allowed origins",
+			"network_type":             "invalid network type provided. Expected one of these values: pubnet or testnet",
+			"sdp_ui_base_url":          "invalid SDP UI base URL value",
+			"sep10_signing_public_key": "invalid public key",
+			"distribution_public_key":  "invalid public key",
+		}, tv.Errors)
+	})
+
+	t.Run("returns error when name is invalid", func(t *testing.T) {
+		tv := NewTenantValidator()
+		reqBody := &TenantRequest{
+			Name:                  "aid org",
+			OwnerEmail:            "owner@email.org",
+			OwnerFirstName:        "Owner",
+			OwnerLastName:         "Owner",
+			OrganizationName:      "Aid Org",
+			NetworkType:           "pubnet",
+			EmailSenderType:       tenant.AWSEmailSenderType,
+			SMSSenderType:         tenant.TwilioSMSSenderType,
+			SEP10SigningPublicKey: keypair.MustRandom().Address(),
+			DistributionPublicKey: keypair.MustRandom().Address(),
+			EnableMFA:             true,
+			EnableReCAPTCHA:       true,
+			CORSAllowedOrigins:    []string{"*"},
+			SDPUIBaseURL:          "http://localhost:3000",
+			BaseURL:               "http://localhost:8000",
+		}
+
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{
+			"name": "invalid tenant name. It should only contains lower case letters and dash (-)",
+		}, tv.Errors)
+	})
+
+	t.Run("returns error when owner info is invalid", func(t *testing.T) {
+		tv := NewTenantValidator()
+		reqBody := &TenantRequest{
+			Name:                  "aid-org",
+			OwnerEmail:            "invalid",
+			OwnerFirstName:        "",
+			OwnerLastName:         "",
+			OrganizationName:      "",
+			NetworkType:           "pubnet",
+			EmailSenderType:       tenant.AWSEmailSenderType,
+			SMSSenderType:         tenant.TwilioSMSSenderType,
+			SEP10SigningPublicKey: keypair.MustRandom().Address(),
+			DistributionPublicKey: keypair.MustRandom().Address(),
+			EnableMFA:             true,
+			EnableReCAPTCHA:       true,
+			CORSAllowedOrigins:    []string{"*"},
+			BaseURL:               "http://localhost:8000",
+			SDPUIBaseURL:          "http://localhost:3000",
+		}
+
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{
+			"owner_email":       "invalid email",
+			"owner_first_name":  "owner_first_name is required",
+			"owner_last_name":   "owner_last_name is required",
+			"organization_name": "organization_name is required",
+		}, tv.Errors)
+
+		reqBody.OwnerEmail = "owner@email.org"
+		reqBody.OwnerFirstName = "Owner"
+		reqBody.OwnerLastName = "Owner"
+		reqBody.OrganizationName = "Aid Org"
+		tv.Errors = map[string]interface{}{}
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.False(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{}, tv.Errors)
+	})
+
+	t.Run("validates the network type successfully", func(t *testing.T) {
+		tv := NewTenantValidator()
+		reqBody := &TenantRequest{
+			Name:                  "aid-org",
+			OwnerEmail:            "owner@email.org",
+			OwnerFirstName:        "Owner",
+			OwnerLastName:         "Owner",
+			OrganizationName:      "Aid Org",
+			NetworkType:           "invalid",
+			EmailSenderType:       tenant.AWSEmailSenderType,
+			SMSSenderType:         tenant.TwilioSMSSenderType,
+			SEP10SigningPublicKey: keypair.MustRandom().Address(),
+			DistributionPublicKey: keypair.MustRandom().Address(),
+			EnableMFA:             true,
+			EnableReCAPTCHA:       true,
+			CORSAllowedOrigins:    []string{"*"},
+			SDPUIBaseURL:          "http://localhost:3000",
+			BaseURL:               "http://localhost:8000",
+		}
+
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{"network_type": "invalid network type provided. Expected one of these values: pubnet or testnet"}, tv.Errors)
+
+		reqBody.NetworkType = "pubnet"
+		tv.Errors = map[string]interface{}{}
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.False(t, tv.HasErrors())
+
+		reqBody.NetworkType = "testnet"
+		tv.Errors = map[string]interface{}{}
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.False(t, tv.HasErrors())
+	})
+
+	t.Run("validates the email sender type successfully", func(t *testing.T) {
+		tv := NewTenantValidator()
+		reqBody := &TenantRequest{
+			Name:                  "aid-org",
+			OwnerEmail:            "owner@email.org",
+			OwnerFirstName:        "Owner",
+			OwnerLastName:         "Owner",
+			OrganizationName:      "Aid Org",
+			NetworkType:           "pubnet",
+			EmailSenderType:       "invalid",
+			SMSSenderType:         tenant.TwilioSMSSenderType,
+			SEP10SigningPublicKey: keypair.MustRandom().Address(),
+			DistributionPublicKey: keypair.MustRandom().Address(),
+			EnableMFA:             true,
+			EnableReCAPTCHA:       true,
+			CORSAllowedOrigins:    []string{"*"},
+			SDPUIBaseURL:          "http://localhost:3000",
+			BaseURL:               "http://localhost:8000",
+		}
+
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{"email_sender_type": "invalid email sender type. Expected one of these values: [AWS_EMAIL DRY_RUN]"}, tv.Errors)
+
+		reqBody.EmailSenderType = tenant.DryRunEmailSenderType
+		tv.Errors = map[string]interface{}{}
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.False(t, tv.HasErrors())
+	})
+
+	t.Run("validates the sms sender type successfully", func(t *testing.T) {
+		tv := NewTenantValidator()
+		reqBody := &TenantRequest{
+			Name:                  "aid-org",
+			OwnerEmail:            "owner@email.org",
+			OwnerFirstName:        "Owner",
+			OwnerLastName:         "Owner",
+			OrganizationName:      "Aid Org",
+			NetworkType:           "pubnet",
+			EmailSenderType:       tenant.AWSEmailSenderType,
+			SMSSenderType:         "invalid",
+			SEP10SigningPublicKey: keypair.MustRandom().Address(),
+			DistributionPublicKey: keypair.MustRandom().Address(),
+			EnableMFA:             true,
+			EnableReCAPTCHA:       true,
+			CORSAllowedOrigins:    []string{"*"},
+			SDPUIBaseURL:          "http://localhost:3000",
+			BaseURL:               "http://localhost:8000",
+		}
+
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{"sms_sender_type": "invalid sms sender type. Expected one of these values: [TWILIO_SMS AWS_SMS DRY_RUN]"}, tv.Errors)
+
+		reqBody.SMSSenderType = tenant.DryRunSMSSenderType
+		tv.Errors = map[string]interface{}{}
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.False(t, tv.HasErrors())
+	})
+
+	t.Run("validates the public keys successfully", func(t *testing.T) {
+		tv := NewTenantValidator()
+		reqBody := &TenantRequest{
+			Name:                  "aid-org",
+			OwnerEmail:            "owner@email.org",
+			OwnerFirstName:        "Owner",
+			OwnerLastName:         "Owner",
+			OrganizationName:      "Aid Org",
+			NetworkType:           "pubnet",
+			EmailSenderType:       tenant.AWSEmailSenderType,
+			SMSSenderType:         tenant.TwilioSMSSenderType,
+			SEP10SigningPublicKey: "invalid",
+			DistributionPublicKey: "invalid",
+			EnableMFA:             true,
+			EnableReCAPTCHA:       true,
+			CORSAllowedOrigins:    []string{"*"},
+			SDPUIBaseURL:          "http://localhost:3000",
+			BaseURL:               "http://localhost:8000",
+		}
+
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{
+			"sep10_signing_public_key": "invalid public key",
+			"distribution_public_key":  "invalid public key",
+		}, tv.Errors)
+	})
+
+	t.Run("validates the URLs successfully", func(t *testing.T) {
+		tv := NewTenantValidator()
+		reqBody := &TenantRequest{
+			Name:                  "aid-org",
+			OwnerEmail:            "owner@email.org",
+			OwnerFirstName:        "Owner",
+			OwnerLastName:         "Owner",
+			OrganizationName:      "Aid Org",
+			NetworkType:           "pubnet",
+			EmailSenderType:       tenant.AWSEmailSenderType,
+			SMSSenderType:         tenant.TwilioSMSSenderType,
+			SEP10SigningPublicKey: keypair.MustRandom().Address(),
+			DistributionPublicKey: keypair.MustRandom().Address(),
+			EnableMFA:             true,
+			EnableReCAPTCHA:       true,
+			CORSAllowedOrigins:    []string{"http://valid.com", "%invalid%"},
+			SDPUIBaseURL:          "%invalid%",
+			BaseURL:               "%invalid%",
+		}
+
+		tv.ValidateCreateTenantRequest(reqBody)
+		assert.True(t, tv.HasErrors())
+		assert.Equal(t, map[string]interface{}{
+			"base_url":             "invalid base URL value",
+			"sdp_ui_base_url":      "invalid SDP UI base URL value",
+			"cors_allowed_origins": "invalid URL value for cors_allowed_origins[1]=%invalid%",
+		}, tv.Errors)
+	})
+}
