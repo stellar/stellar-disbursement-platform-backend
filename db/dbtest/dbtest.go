1a4bc7d374f967e8d4848a7b2a4cc53ff51aeb4b
Marcelo Salloum <marcelo@stellar.org>
2023-12-13 15:31:09 -0800

    Fix imports.

--- db/dbtest/dbtest.go
+++ db/dbtest/dbtest.go
@@ -7,9 +7,9 @@ import (
 	migrate "github.com/rubenv/sql-migrate"
 	"github.com/stellar/go/support/db/dbtest"
 	"github.com/stellar/go/support/db/schema"
+	adminmigrations "github.com/stellar/stellar-disbursement-platform-backend/db/migrations/admin-migrations"
 	authmigrations "github.com/stellar/stellar-disbursement-platform-backend/db/migrations/auth-migrations"
 	sdpmigrations "github.com/stellar/stellar-disbursement-platform-backend/db/migrations/sdp-migrations"
-	tenantmigrations "github.com/stellar/stellar-disbursement-platform-backend/db/migrations/tenant-migrations"
 )
 
 func OpenWithoutMigrations(t *testing.T) *dbtest.DB {
@@ -26,7 +26,7 @@ func Open(t *testing.T) *dbtest.DB {
 	// Tenant migrations
 	ms := migrate.MigrationSet{TableName: "migrations"}
 	migrateDirection := migrate.Up
-	m := migrate.HttpFileSystemMigrationSource{FileSystem: http.FS(tenantmigrations.FS)}
+	m := migrate.HttpFileSystemMigrationSource{FileSystem: http.FS(adminmigrations.FS)}
 	_, err := ms.ExecMax(conn.DB, "postgres", m, migrateDirection, 0)
 	if err != nil {
 		t.Fatal(err)
@@ -57,7 +57,7 @@ func OpenWithTenantMigrationsOnly(t *testing.T) *dbtest.DB {
 	defer conn.Close()
 
 	migrateDirection := schema.MigrateUp
-	m := migrate.HttpFileSystemMigrationSource{FileSystem: http.FS(tenantmigrations.FS)}
+	m := migrate.HttpFileSystemMigrationSource{FileSystem: http.FS(adminmigrations.FS)}
 	_, err := schema.Migrate(conn.DB, m, migrateDirection, 0)
 	if err != nil {
 		t.Fatal(err)
