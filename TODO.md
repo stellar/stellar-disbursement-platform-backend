a72d6e751183d37a2f69be9edd75e27e68fcb132
Marcelo Salloum <marcelo@stellar.org>
2024-01-25 13:47:56 -0800

    Add tests for dependency injection's signature_service.

--- TODO.md
+++ TODO.md
@@ -7,8 +7,8 @@ As part of this PR, don't forget to:
 - [x] Update channel-account view method signature. No options object is needed
 - [x] create/ensure commands now don't use flags anymore. Handle it in the helm chart
 - [x] handle helmchart and stellar/kube around the variable `channel-account-encryption-passphrase`
-- [ ] Add tests for dependency injectors
+- [⏳] Add tests for dependency injectors
   - [ ] tss_db_connection_pool
   - [ ] signature_service
-  - [ ] New methods in channel_accounts_service.go
+  - [x] New methods in channel_accounts_service.go
 - [ ] Add documentation saying to use `go run main.go channel-accounts delete --delete-all-accounts --channel-account-encryption-key=$DISTRIBUTION_SEED` to delete all previous accounts, and then recreate them with ``go run main.go channel-accounts ensure --num-channel-accounts-ensure={whatever}`
---
+++ internal/dependencyinjection/signature_service_test.go
@@ -0,0 +1,70 @@
+package dependencyinjection
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"github.com/stellar/stellar-disbursement-platform-backend/db"
+	"github.com/stellar/stellar-disbursement-platform-backend/db/dbtest"
+	"github.com/stellar/stellar-disbursement-platform-backend/internal/transactionsubmission/engine"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_dependencyinjection_NewSignatureService(t *testing.T) {
+	dbt := dbtest.Open(t)
+	defer dbt.Close()
+	dbConnectionPool, err := db.OpenDBConnectionPool(dbt.DSN)
+	require.NoError(t, err)
+	defer dbConnectionPool.Close()
+
+	distributionPrivateKey := keypair.MustRandom().Seed()
+	encryptionPassphrase := keypair.MustRandom().Seed()
+
+	ctx := context.Background()
+	t.Run("should create and return the same instance on the second call", func(t *testing.T) {
+		ClearInstancesTestHelper(t)
+
+		opts := engine.DefaultSignatureServiceOptions{
+			NetworkPassphrase:      network.TestNetworkPassphrase,
+			DBConnectionPool:       dbConnectionPool,
+			DistributionPrivateKey: distributionPrivateKey,
+			EncryptionPassphrase:   encryptionPassphrase,
+		}
+
+		gotDependency, err := NewSignatureService(ctx, opts)
+		require.NoError(t, err)
+
+		gotDependencyDuplicate, err := NewSignatureService(ctx, opts)
+		require.NoError(t, err)
+
+		assert.Equal(t, &gotDependency, &gotDependencyDuplicate)
+	})
+
+	t.Run("should return an error on a invalid option", func(t *testing.T) {
+		ClearInstancesTestHelper(t)
+
+		opts := engine.DefaultSignatureServiceOptions{}
+		gotDependency, err := NewSignatureService(ctx, opts)
+		assert.Nil(t, gotDependency)
+		assert.EqualError(t, err, "creating a new signature service instance: validating options: network passphrase cannot be empty")
+	})
+
+	t.Run("should return an error if there's an invalid instance pre-stored", func(t *testing.T) {
+		ClearInstancesTestHelper(t)
+
+		setInstance(signatureServiceInstanceName, false)
+
+		opts := engine.DefaultSignatureServiceOptions{
+			NetworkPassphrase:      network.TestNetworkPassphrase,
+			DBConnectionPool:       dbConnectionPool,
+			DistributionPrivateKey: distributionPrivateKey,
+			EncryptionPassphrase:   encryptionPassphrase,
+		}
+		gotDependency, err := NewSignatureService(ctx, opts)
+		assert.Nil(t, gotDependency)
+		assert.EqualError(t, err, "trying to cast an existing signature service instance")
+	})
+}
