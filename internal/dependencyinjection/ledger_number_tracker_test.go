b2f702bb10741eacbed41137bbfee8f07ce72d57
Marcelo Salloum <marcelo@stellar.org>
2024-01-31 14:25:57 -0800

    Add missing tests for the dependencyinjection package.

---
+++ internal/dependencyinjection/ledger_number_tracker_test.go
@@ -0,0 +1,40 @@
+package dependencyinjection
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/stellar-disbursement-platform-backend/db/dbtest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_dependencyinjection_NewLedgerNumberTracker(t *testing.T) {
+	dbt := dbtest.Open(t)
+	defer dbt.Close()
+
+	ctx := context.Background()
+	t.Run("should create and return the same instance on the second call", func(t *testing.T) {
+		ClearInstancesTestHelper(t)
+
+		horizonClient := &horizonclient.MockClient{}
+		gotDependency, err := NewLedgerNumberTracker(ctx, horizonClient)
+		require.NoError(t, err)
+
+		gotDependencyDuplicate, err := NewLedgerNumberTracker(ctx, horizonClient)
+		require.NoError(t, err)
+
+		assert.Equal(t, &gotDependency, &gotDependencyDuplicate)
+	})
+
+	t.Run("should return an error if there's an invalid instance pre-stored", func(t *testing.T) {
+		ClearInstancesTestHelper(t)
+
+		SetInstance(ledgerNumberTrackerInstanceName, false)
+
+		gotDependency, err := NewLedgerNumberTracker(ctx, &horizonclient.MockClient{})
+		assert.Nil(t, gotDependency)
+		assert.EqualError(t, err, "trying to cast an existing ledger number tracker instance")
+	})
+}
---
+++ internal/dependencyinjection/tx_submitter_engine_test.go
@@ -0,0 +1,45 @@
+package dependencyinjection
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/stellar/stellar-disbursement-platform-backend/db/dbtest"
+	engineMocks "github.com/stellar/stellar-disbursement-platform-backend/internal/transactionsubmission/engine/mocks"
+)
+
+func Test_dependencyinjection_NewTxSubmitterEngine(t *testing.T) {
+	dbt := dbtest.Open(t)
+	defer dbt.Close()
+
+	ctx := context.Background()
+	t.Run("should create and return the same instance on the second call", func(t *testing.T) {
+		ClearInstancesTestHelper(t)
+
+		mSigService := engineMocks.NewMockSignatureService(t)
+		SetInstance(signatureServiceInstanceName, mSigService)
+
+		opts := TxSubmitterEngineOptions{MaxBaseFee: 100}
+		gotDependency, err := NewTxSubmitterEngine(ctx, opts)
+		require.NoError(t, err)
+
+		gotDependencyDuplicate, err := NewTxSubmitterEngine(ctx, opts)
+		require.NoError(t, err)
+
+		assert.Equal(t, &gotDependency, &gotDependencyDuplicate)
+	})
+
+	t.Run("should return an error if there's an invalid instance pre-stored", func(t *testing.T) {
+		ClearInstancesTestHelper(t)
+
+		SetInstance(txSubmitterEngineInstanceName, false)
+
+		opts := TxSubmitterEngineOptions{}
+		gotDependency, err := NewTxSubmitterEngine(ctx, opts)
+		assert.Empty(t, gotDependency)
+		assert.EqualError(t, err, "trying to cast an existing submitter engine instance")
+	})
+}
