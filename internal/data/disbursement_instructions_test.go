a1795825ef20fe20c9672a4160ee56385faffb04
Marcelo Salloum dos Santos <marcelo@stellar.org>
2024-02-21 08:59:55 -0800

    [Chore] Bump go version and make other project maintenance changes (#196)
    ### What
    
    - Bump go version from 1.19 (released 2022-08-02) to 1.22 (released 2024-02-06).
    - Updated the version of some github actions
    - Updated `slices` dependency to use the one that's now part of the Go SDK.
    - Removed two unused methods
    - Apply `deadcode` to the CI, to make sure there's no unused code in the project.
    
    ### Why
    
    We were falling behind on the version upgrade.
    
    The other bullets are also project maintenance.

--- internal/data/disbursement_instructions_test.go
+++ internal/data/disbursement_instructions_test.go
@@ -276,7 +276,7 @@ func Test_DisbursementInstructionModel_ProcessAll(t *testing.T) {
 		instruction3.VerificationValue = "1990-01-07"
 		err = di.ProcessAll(ctx, "user-id", instructions, disbursement, disbursementUpdate, MaxInstructionsPerDisbursement)
 		require.Error(t, err)
-		assert.EqualError(t, err, "running atomic function in RunInTransactionWithResult: receiver verification mismatch: receiver verification for +380-12-345-673 doesn't match. Check line 3 on CSV file - Internal ID 123456783.")
+		assert.EqualError(t, err, "running atomic function in RunInTransactionWithResult: receiver verification mismatch: receiver verification for +380-12-345-673 doesn't match. Check line 3 on CSV file - Internal ID 123456783")
 	})
 }
 
