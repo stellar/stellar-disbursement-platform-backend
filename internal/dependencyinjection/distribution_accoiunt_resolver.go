6516b62236cb31be3f44e1713ecffb76ccd796df
Marcelo Salloum <marcelo@stellar.org>
2024-03-01 13:51:59 -0800

    Add method dependencyinjection.NewDistributionAccountResolver

---
+++ internal/dependencyinjection/distribution_accoiunt_resolver.go
@@ -0,0 +1,36 @@
+package dependencyinjection
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/stellar/go/support/log"
+
+	"github.com/stellar/stellar-disbursement-platform-backend/internal/transactionsubmission/engine/signing"
+)
+
+const DistributionAccountResolverInstanceName = "distribution_account_resolver_instance"
+
+// NewSignatureService creates a new distribution account resolver instance, or retrieves an instance that was already
+// created before.
+func NewDistributionAccountResolver(ctx context.Context, opts signing.DistributionAccountResolverOptions) (signing.DistributionAccountResolver, error) {
+	instanceName := DistributionAccountResolverInstanceName
+
+	// Already initialized
+	if instance, ok := GetInstance(instanceName); ok {
+		if distAccResolverInstance, ok2 := instance.(signing.DistributionAccountResolver); ok2 {
+			return distAccResolverInstance, nil
+		}
+		return nil, fmt.Errorf("trying to cast an existing distribution account resolver instance")
+	}
+
+	log.Ctx(ctx).Info("⚙️ Setting up Distribution Account Resolver")
+	newInstance, err := signing.NewDistributionAccountResolver(opts)
+	if err != nil {
+		return nil, fmt.Errorf("creating a new distribution account resolver instance: %w", err)
+	}
+
+	SetInstance(instanceName, newInstance)
+
+	return newInstance, nil
+}
