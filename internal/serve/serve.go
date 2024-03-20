a46cddc2f772f032faf8b0aa493f8d32783fe9ee
Cecília Romão <ceciliaromao@gmail.com>
2024-03-20 15:46:46 -0300

    fix: InjectTenant after Authenticating

--- internal/serve/serve.go
+++ internal/serve/serve.go
@@ -204,7 +204,6 @@ func handleHTTP(o ServeOptions) *chi.Mux {
 	))
 	mux.Use(chimiddleware.RequestID)
 	mux.Use(chimiddleware.RealIP)
-	mux.Use(middleware.InjectTenantMiddleware(o.tenantManager, o.authManager))
 	mux.Use(middleware.LoggingMiddleware)
 	mux.Use(middleware.RecoverHandler)
 	mux.Use(middleware.MetricsRequestHandler(o.MonitorService))
@@ -219,6 +218,7 @@ func handleHTTP(o ServeOptions) *chi.Mux {
 	authManager := o.authManager
 	mux.Group(func(r chi.Router) {
 		r.Use(middleware.AuthenticateMiddleware(authManager))
+		r.Use(middleware.InjectTenantMiddleware(o.tenantManager, o.authManager))
 		r.Use(middleware.EnsureTenantMiddleware)
 
 		r.With(middleware.AnyRoleMiddleware(authManager, data.GetAllRoles()...)).Route("/statistics", func(r chi.Router) {
@@ -373,6 +373,7 @@ func handleHTTP(o ServeOptions) *chi.Mux {
 
 	// Public routes that are tenant aware (they need to know the tenant ID)
 	mux.Group(func(r chi.Router) {
+		r.Use(middleware.InjectTenantMiddleware(o.tenantManager, o.authManager))
 		r.Use(middleware.EnsureTenantMiddleware)
 
 		r.Post("/login", httphandler.LoginHandler{
@@ -404,6 +405,7 @@ func handleHTTP(o ServeOptions) *chi.Mux {
 
 	// SEP-24 and miscellaneous endpoints that are tenant-unaware
 	mux.Group(func(r chi.Router) {
+		r.Use(middleware.InjectTenantMiddleware(o.tenantManager, o.authManager))
 		r.Get("/health", httphandler.HealthHandler{
 			ReleaseID: o.GitCommit,
 			ServiceID: ServiceID,
