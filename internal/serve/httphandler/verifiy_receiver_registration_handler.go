25cf58fb23cb2533849033e692bbe9efb194f7d1
Marcelo Salloum dos Santos <marcelo@stellar.org>
2024-04-15 06:19:18 -0700

    Chore: fix main.sh (#257)
    ### What
    
    Fix main.sh
    
    ### Why
    
    The check for existing tenants breaks when there are no existing tenants.

--- internal/serve/httphandler/verifiy_receiver_registration_handler.go
+++ internal/serve/httphandler/verifiy_receiver_registration_handler.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/stellar/go/support/log"
 	"github.com/stellar/go/support/render/httpjson"
+
 	"github.com/stellar/stellar-disbursement-platform-backend/db"
 	"github.com/stellar/stellar-disbursement-platform-backend/internal/anchorplatform"
 	"github.com/stellar/stellar-disbursement-platform-backend/internal/data"
