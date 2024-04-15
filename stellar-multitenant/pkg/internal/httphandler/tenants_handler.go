41d5e829041f4c77cb25a5f0fe3cca26356fb0e8
Marcelo Salloum dos Santos <marcelo@stellar.org>
2024-04-15 06:35:19 -0700

    [Chore] Update default signer type to `DISTRIBUTION_ACCOUNT_DB` (#254)
    ### What
    
    Update default signer type to `DISTRIBUTION_ACCOUNT_DB`
    
    ### Why
    
    This is the most recommended setup for multitenancy and works great with single tenant as well.

--- stellar-multitenant/pkg/internal/httphandler/tenants_handler.go
+++ stellar-multitenant/pkg/internal/httphandler/tenants_handler.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stellar/go/support/http/httpdecode"
 	"github.com/stellar/go/support/log"
 	"github.com/stellar/go/support/render/httpjson"
+
 	"github.com/stellar/stellar-disbursement-platform-backend/db"
 	"github.com/stellar/stellar-disbursement-platform-backend/internal/serve/httperror"
 	"github.com/stellar/stellar-disbursement-platform-backend/internal/utils"
