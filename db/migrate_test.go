f6735adfb7be63d14e3b0aefa3f9178598b70db1
Marcelo Salloum <marcelo@stellar.org>
2024-04-18 19:04:17 -0300

    Reduce repetition in migration tests.

--- db/migrate_test.go
+++ db/migrate_test.go
@@ -14,319 +14,144 @@ import (
 	"github.com/stellar/stellar-disbursement-platform-backend/db/migrations"
 )
 
-func TestMigrate_upApplyOne_SDP_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
+func TestMigrate_up_1(t *testing.T) {
+	testCases := []struct {
+		migrationRouter      migrations.MigrationRouter
+		initialMigrationName string
+	}{
+		{
+			migrationRouter:      migrations.SDPMigrationRouter,
+			initialMigrationName: "2023-01-20.0-initial.sql",
+		},
+		{
+			migrationRouter:      migrations.AdminMigrationRouter,
+			initialMigrationName: "2023-10-16.0.add-tenants-table.sql",
+		},
+		{
+			migrationRouter:      migrations.AuthMigrationRouter,
+			initialMigrationName: "2023-02-09.0.add-users-table.sql",
+		},
+		{
+			migrationRouter:      migrations.TSSMigrationRouter,
+			initialMigrationName: "2024-01-03.0-add-submitter-transactions-table.sql",
+		},
+	}
 
-	ctx := context.Background()
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%s-up-1", tc.migrationRouter.TableName), func(t *testing.T) {
+			db := dbtest.OpenWithoutMigrations(t)
+			defer db.Close()
+			dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
+			require.NoError(t, err)
+			defer dbConnectionPool.Close()
 
-	n, err := Migrate(db.DSN, migrate.Up, 1, migrations.SDPMigrationRouter)
-	require.NoError(t, err)
-	assert.Equal(t, 1, n)
+			ctx := context.Background()
 
-	ids := []string{}
-	err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", migrations.SDPMigrationRouter.TableName))
-	require.NoError(t, err)
-	wantIDs := []string{"2023-01-20.0-initial.sql"}
-	assert.Equal(t, wantIDs, ids)
+			n, err := Migrate(db.DSN, migrate.Up, 1, tc.migrationRouter)
+			require.NoError(t, err)
+			assert.Equal(t, 1, n)
+
+			ids := []string{}
+			err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", tc.migrationRouter.TableName))
+			require.NoError(t, err)
+			wantIDs := []string{tc.initialMigrationName}
+			assert.Equal(t, wantIDs, ids)
+		})
+	}
 }
 
-func TestMigrate_downApplyOne_SDP_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
+func TestMigrate_up_2_down_1(t *testing.T) {
+	testCases := []struct {
+		migrationRouter      migrations.MigrationRouter
+		initialMigrationName string
+	}{
+		{
+			migrationRouter:      migrations.SDPMigrationRouter,
+			initialMigrationName: "2023-01-20.0-initial.sql",
+		},
+		{
+			migrationRouter:      migrations.AdminMigrationRouter,
+			initialMigrationName: "2023-10-16.0.add-tenants-table.sql",
+		},
+		{
+			migrationRouter:      migrations.AuthMigrationRouter,
+			initialMigrationName: "2023-02-09.0.add-users-table.sql",
+		},
+		{
+			migrationRouter:      migrations.TSSMigrationRouter,
+			initialMigrationName: "2024-01-03.0-add-submitter-transactions-table.sql",
+		},
+	}
 
-	ctx := context.Background()
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%s-up-2--down-1", tc.migrationRouter.TableName), func(t *testing.T) {
+			db := dbtest.OpenWithoutMigrations(t)
+			defer db.Close()
+			dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
+			require.NoError(t, err)
+			defer dbConnectionPool.Close()
 
-	n, err := Migrate(db.DSN, migrate.Up, 2, migrations.SDPMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, 2, n)
+			ctx := context.Background()
 
-	n, err = Migrate(db.DSN, migrate.Down, 1, migrations.SDPMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, 1, n)
+			n, err := Migrate(db.DSN, migrate.Up, 2, tc.migrationRouter)
+			require.NoError(t, err)
+			require.Equal(t, 2, n)
 
-	ids := []string{}
-	err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", migrations.SDPMigrationRouter.TableName))
-	require.NoError(t, err)
-	wantIDs := []string{"2023-01-20.0-initial.sql"}
-	assert.Equal(t, wantIDs, ids)
+			n, err = Migrate(db.DSN, migrate.Down, 1, tc.migrationRouter)
+			require.NoError(t, err)
+			require.Equal(t, 1, n)
+
+			ids := []string{}
+			err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", tc.migrationRouter.TableName))
+			require.NoError(t, err)
+			wantIDs := []string{tc.initialMigrationName}
+			assert.Equal(t, wantIDs, ids)
+		})
+	}
 }
 
-func TestMigrate_upAndDownAllTheWayTwice_SDP_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
+func TestMigrate_upAndDownAllTheWayTwice(t *testing.T) {
+	migrationRouters := []migrations.MigrationRouter{
+		migrations.SDPMigrationRouter,
+		migrations.AdminMigrationRouter,
+		migrations.AuthMigrationRouter,
+		migrations.TSSMigrationRouter,
+	}
 
-	// Get number of files in the migrations directory:
-	var count int
-	err = fs.WalkDir(migrations.SDPMigrationRouter.FS, ".", func(path string, d fs.DirEntry, err error) error {
-		require.NoError(t, err)
-		if !d.IsDir() {
-			count++
-		}
-		return nil
-	})
-	require.NoError(t, err)
+	for _, migrationRouter := range migrationRouters {
+		t.Run(fmt.Sprintf("%s-up-and-down-all-the-way-twice", migrationRouter.TableName), func(t *testing.T) {
+			db := dbtest.OpenWithoutMigrations(t)
+			defer db.Close()
+			dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
+			require.NoError(t, err)
+			defer dbConnectionPool.Close()
 
-	n, err := Migrate(db.DSN, migrate.Up, count, migrations.SDPMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
+			// Get number of files in the migrations directory:
+			var count int
+			err = fs.WalkDir(migrationRouter.FS, ".", func(path string, d fs.DirEntry, err error) error {
+				require.NoError(t, err)
+				if !d.IsDir() {
+					count++
+				}
+				return nil
+			})
+			require.NoError(t, err)
 
-	n, err = Migrate(db.DSN, migrate.Down, count, migrations.SDPMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
+			n, err := Migrate(db.DSN, migrate.Up, count, migrationRouter)
+			require.NoError(t, err)
+			require.Equal(t, count, n)
 
-	n, err = Migrate(db.DSN, migrate.Up, count, migrations.SDPMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
+			n, err = Migrate(db.DSN, migrate.Down, count, migrationRouter)
+			require.NoError(t, err)
+			require.Equal(t, count, n)
 
-	n, err = Migrate(db.DSN, migrate.Down, count, migrations.SDPMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-}
+			n, err = Migrate(db.DSN, migrate.Up, count, migrationRouter)
+			require.NoError(t, err)
+			require.Equal(t, count, n)
 
-func TestMigrate_upApplyOne_Tenant_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
-
-	ctx := context.Background()
-
-	n, err := Migrate(db.DSN, migrate.Up, 1, migrations.AdminMigrationRouter)
-	require.NoError(t, err)
-	assert.Equal(t, 1, n)
-
-	ids := []string{}
-	err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", migrations.AdminMigrationRouter.TableName))
-	require.NoError(t, err)
-	wantIDs := []string{"2023-10-16.0.add-tenants-table.sql"}
-	assert.Equal(t, wantIDs, ids)
-}
-
-func TestMigrate_downApplyTwo_Tenant_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
-
-	ctx := context.Background()
-
-	n, err := Migrate(db.DSN, migrate.Up, 2, migrations.AdminMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, 2, n)
-
-	n, err = Migrate(db.DSN, migrate.Down, 2, migrations.AdminMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, 2, n)
-
-	ids := []string{}
-	err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", migrations.AdminMigrationRouter.TableName))
-	require.NoError(t, err)
-	wantIDs := []string{}
-	assert.Equal(t, wantIDs, ids)
-}
-
-func TestMigrate_upAndDownAllTheWayTwice_Tenant_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
-
-	// Get number of files in the migrations directory:
-	var count int
-	err = fs.WalkDir(migrations.AdminMigrationRouter.FS, ".", func(path string, d fs.DirEntry, err error) error {
-		require.NoError(t, err)
-		if !d.IsDir() {
-			count++
-		}
-		return nil
-	})
-	require.NoError(t, err)
-
-	n, err := Migrate(db.DSN, migrate.Up, count, migrations.AdminMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-
-	n, err = Migrate(db.DSN, migrate.Down, count, migrations.AdminMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-
-	n, err = Migrate(db.DSN, migrate.Up, count, migrations.AdminMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-
-	n, err = Migrate(db.DSN, migrate.Down, count, migrations.AdminMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-}
-
-func TestMigrate_upApplyOne_Auth_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
-
-	ctx := context.Background()
-
-	n, err := Migrate(db.DSN, migrate.Up, 1, migrations.AuthMigrationRouter)
-	require.NoError(t, err)
-	assert.Equal(t, 1, n)
-
-	ids := []string{}
-	err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", migrations.AuthMigrationRouter.TableName))
-	require.NoError(t, err)
-	wantIDs := []string{"2023-02-09.0.add-users-table.sql"}
-	assert.Equal(t, wantIDs, ids)
-}
-
-func TestMigrate_downApplyOne_Auth_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
-
-	ctx := context.Background()
-
-	n, err := Migrate(db.DSN, migrate.Up, 2, migrations.AuthMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, 2, n)
-
-	n, err = Migrate(db.DSN, migrate.Down, 1, migrations.AuthMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, 1, n)
-
-	ids := []string{}
-	err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", migrations.AuthMigrationRouter.TableName))
-	require.NoError(t, err)
-	wantIDs := []string{"2023-02-09.0.add-users-table.sql"}
-	assert.Equal(t, wantIDs, ids)
-}
-
-func TestMigrate_upAndDownAllTheWayTwice_Auth_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
-
-	// Get number of files in the migrations directory:
-	var count int
-	err = fs.WalkDir(migrations.AuthMigrationRouter.FS, ".", func(path string, d fs.DirEntry, err error) error {
-		require.NoError(t, err)
-		if !d.IsDir() {
-			count++
-		}
-		return nil
-	})
-	require.NoError(t, err)
-
-	n, err := Migrate(db.DSN, migrate.Up, count, migrations.AuthMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-
-	n, err = Migrate(db.DSN, migrate.Down, count, migrations.AuthMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-
-	n, err = Migrate(db.DSN, migrate.Up, count, migrations.AuthMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-
-	n, err = Migrate(db.DSN, migrate.Down, count, migrations.AuthMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-}
-
-func TestMigrate_upApplyOne_TSS_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
-
-	ctx := context.Background()
-
-	n, err := Migrate(db.DSN, migrate.Up, 1, migrations.TSSMigrationRouter)
-	require.NoError(t, err)
-	assert.Equal(t, 1, n)
-
-	ids := []string{}
-	err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", migrations.TSSMigrationRouter.TableName))
-	require.NoError(t, err)
-	wantIDs := []string{"2024-01-03.0-add-submitter-transactions-table.sql"}
-	assert.Equal(t, wantIDs, ids)
-}
-
-func TestMigrate_downApplyTwo_TSS_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
-
-	ctx := context.Background()
-
-	n, err := Migrate(db.DSN, migrate.Up, 2, migrations.TSSMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, 2, n)
-
-	n, err = Migrate(db.DSN, migrate.Down, 2, migrations.TSSMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, 2, n)
-
-	ids := []string{}
-	err = dbConnectionPool.SelectContext(ctx, &ids, fmt.Sprintf("SELECT id FROM %s", migrations.TSSMigrationRouter.TableName))
-	require.NoError(t, err)
-	wantIDs := []string{}
-	assert.Equal(t, wantIDs, ids)
-}
-
-func TestMigrate_upAndDownAllTheWayTwice_TSS_migrations(t *testing.T) {
-	db := dbtest.OpenWithoutMigrations(t)
-	defer db.Close()
-	dbConnectionPool, err := OpenDBConnectionPool(db.DSN)
-	require.NoError(t, err)
-	defer dbConnectionPool.Close()
-
-	// Get number of files in the migrations directory:
-	var count int
-
-	err = fs.WalkDir(migrations.TSSMigrationRouter.FS, ".", func(path string, d fs.DirEntry, err error) error {
-		require.NoError(t, err)
-		if !d.IsDir() {
-			count++
-		}
-		return nil
-	})
-	require.NoError(t, err)
-
-	n, err := Migrate(db.DSN, migrate.Up, count, migrations.TSSMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-
-	n, err = Migrate(db.DSN, migrate.Down, count, migrations.TSSMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-
-	n, err = Migrate(db.DSN, migrate.Up, count, migrations.TSSMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
-
-	n, err = Migrate(db.DSN, migrate.Down, count, migrations.TSSMigrationRouter)
-	require.NoError(t, err)
-	require.Equal(t, count, n)
+			n, err = Migrate(db.DSN, migrate.Down, count, migrationRouter)
+			require.NoError(t, err)
+			require.Equal(t, count, n)
+		})
+	}
 }
