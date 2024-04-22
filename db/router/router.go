7e45b8df57dee6b61b2a15d0a4f868e1a092dd83
Marcelo Salloum <marcelo@stellar.org>
2024-04-22 12:35:35 -0300

    Improve code reusability when building the DNS per schema.

--- db/router/router.go
+++ db/router/router.go
@@ -14,45 +14,30 @@ const (
 // GetDNSForAdmin returns the database DSN for the Admin schema. It is the same as the root database DSN, clearing the
 // `search_path` if it exists.
 func GetDNSForAdmin(dataSourceName string) (string, error) {
-	u, err := url.Parse(dataSourceName)
-	if err != nil {
-		return "", fmt.Errorf("parsing database DSN: %w", err)
-	}
-
-	q := u.Query()
-	q.Set("search_path", AdminSchemaName)
-	u.RawQuery = q.Encode()
-
-	return u.String(), nil
+	return getDSNWithFixedSchema(dataSourceName, AdminSchemaName)
 }
 
 // GetDSNForTSS returns the database DSN for the TSS schema. It is basically the same as the root database DSN, but
 // with the `search_path` query parameter (AKA schema) set to `tss`.
 func GetDNSForTSS(dataSourceName string) (string, error) {
-	u, err := url.Parse(dataSourceName)
-	if err != nil {
-		return "", fmt.Errorf("parsing database DSN: %w", err)
-	}
-
-	q := u.Query()
-	q.Set("search_path", TSSSchemaName)
-	u.RawQuery = q.Encode()
-
-	return u.String(), nil
+	return getDSNWithFixedSchema(dataSourceName, TSSSchemaName)
 }
 
 // GetDSNForTenant returns the database DSN for the tenant schema. It is basically the same as the root database DSN,
 // but with the `search_path` query parameter (AKA schema) set to `sdp_<tenant_name>`.
 func GetDSNForTenant(dataSourceName, tenantName string) (string, error) {
+	schemaName := fmt.Sprintf("%s%s", SDPSchemaNamePrefix, tenantName)
+	return getDSNWithFixedSchema(dataSourceName, schemaName)
+}
+
+// Helper function to set the search_path for the schema.
+func getDSNWithFixedSchema(dataSourceName, schemaName string) (string, error) {
 	u, err := url.Parse(dataSourceName)
 	if err != nil {
 		return "", fmt.Errorf("parsing database DSN: %w", err)
 	}
-
 	q := u.Query()
-	schemaName := fmt.Sprintf("%s%s", SDPSchemaNamePrefix, tenantName)
 	q.Set("search_path", schemaName)
 	u.RawQuery = q.Encode()
-
 	return u.String(), nil
 }
