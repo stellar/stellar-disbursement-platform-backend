0d3f30c9fd768cc21d65868cf3e6ba8036820dd2
Marcelo Salloum <marcelo@stellar.org>
2024-04-30 11:22:30 -0300

    Polish script.

--- scripts/exclude_from_coverage.sh
+++ scripts/exclude_from_coverage.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 while IFS= read -r p || [ -n "$p" ]; do
   exp=".*${p}.*"
-  grep -v "$exp" c.out > c.out.tmp && mv c.out.tmp c.out
+  grep -v "$exp" c.out > c.out.tmp
 done << EOF # list of terms we want to exclude
 mock
+tss_payments_loadtest.go
 EOF
+
+mv c.out.tmp c.out
