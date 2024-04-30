a190fa290f7ffac240880e957b07394b3d68a189
Marcelo Salloum <marcelo@stellar.org>
2024-04-30 13:34:44 -0300

    Remove redundant http mock.

--- internal/serve/validators/recaptcha_test.go
+++ internal/serve/validators/recaptcha_test.go
@@ -9,70 +9,77 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+
+	"github.com/stellar/stellar-disbursement-platform-backend/internal/serve/httpclient"
 )
 
 func Test_GoogleReCAPTCHAValidator(t *testing.T) {
 	siteSecretKey := "secretKey"
-	httpClientMock := &httpClientMock{}
+	httpClientMock := &httpclient.HttpClientMock{}
 
 	grv := NewGoogleReCAPTCHAValidator(siteSecretKey, httpClientMock)
-
 	ctx := context.Background()
+	const token = "token"
+
 	t.Run("returns error when requesting verify token URL fails", func(t *testing.T) {
-		token := "token"
+		httpClientMock.
+			On("Do", mock.AnythingOfType("*http.Request")).
+			Run(func(args mock.Arguments) {
+				req, ok := args.Get(0).(*http.Request)
+				require.True(t, ok)
 
-		httpClientMock.mockDo = func(req *http.Request) (*http.Response, error) {
-			assert.Equal(t, verifyTokenURL, req.URL.String())
-			assert.Equal(t, http.MethodPost, req.Method)
-			assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
+				require.Equal(t, verifyTokenURL, req.URL.String())
+				require.Equal(t, http.MethodPost, req.Method)
+				require.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
 
-			reqBody, err := io.ReadAll(req.Body)
-			require.NoError(t, err)
-			defer req.Body.Close()
+				reqBody, err := io.ReadAll(req.Body)
+				require.NoError(t, err)
+				defer req.Body.Close()
 
-			assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
-
-			return &http.Response{
-				Body:       io.NopCloser(strings.NewReader("{}")),
-				StatusCode: http.StatusOK,
-			}, fmt.Errorf("unexpected error")
-		}
+				assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
+			}).
+			Return(nil, fmt.Errorf("unexpected error")).
+			Once()
 
 		isValid, err := grv.IsTokenValid(ctx, token)
 
 		assert.False(t, isValid)
 		assert.EqualError(t, err, "error requesting verify reCAPTCHA token: unexpected error")
+		httpClientMock.AssertExpectations(t)
 	})
 
 	t.Run("returns error when an error code is returned", func(t *testing.T) {
-		token := "token"
-
-		httpClientMock.mockDo = func(req *http.Request) (*http.Response, error) {
-			assert.Equal(t, verifyTokenURL, req.URL.String())
-			assert.Equal(t, http.MethodPost, req.Method)
-			assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
-
-			reqBody, err := io.ReadAll(req.Body)
-			require.NoError(t, err)
-			defer req.Body.Close()
-
-			assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
-
-			respBody := `
-				{
-					"success": false,
-					"error-codes": [
-						"bad-request"
-					]
-				}
-			`
-
-			return &http.Response{
-				Body:       io.NopCloser(strings.NewReader(respBody)),
-				StatusCode: http.StatusOK,
-			}, nil
+		respBody := `{
+			"success": false,
+			"error-codes": [
+				"bad-request"
+			]
+		}`
+		response := &http.Response{
+			Body:       io.NopCloser(strings.NewReader(respBody)),
+			StatusCode: http.StatusOK,
 		}
+
+		httpClientMock.
+			On("Do", mock.AnythingOfType("*http.Request")).
+			Run(func(args mock.Arguments) {
+				req, ok := args.Get(0).(*http.Request)
+				require.True(t, ok)
+
+				assert.Equal(t, verifyTokenURL, req.URL.String())
+				assert.Equal(t, http.MethodPost, req.Method)
+				assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
+
+				reqBody, err := io.ReadAll(req.Body)
+				require.NoError(t, err)
+				defer req.Body.Close()
+
+				assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
+			}).
+			Return(response, nil).
+			Once()
 
 		isValid, err := grv.IsTokenValid(ctx, token)
 
@@ -81,34 +88,36 @@ func Test_GoogleReCAPTCHAValidator(t *testing.T) {
 	})
 
 	t.Run("returns false when timeout-or-duplicate error code is returned", func(t *testing.T) {
-		token := "token"
-
-		httpClientMock.mockDo = func(req *http.Request) (*http.Response, error) {
-			assert.Equal(t, verifyTokenURL, req.URL.String())
-			assert.Equal(t, http.MethodPost, req.Method)
-			assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
-
-			reqBody, err := io.ReadAll(req.Body)
-			require.NoError(t, err)
-			defer req.Body.Close()
-
-			assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
-
-			respBody := `
-				{
-					"success": false,
-					"error-codes": [
-						"bad-request",
-						"timeout-or-duplicate"
-					]
-				}
-			`
-
-			return &http.Response{
-				Body:       io.NopCloser(strings.NewReader(respBody)),
-				StatusCode: http.StatusOK,
-			}, nil
+		respBody := `{
+			"success": false,
+			"error-codes": [
+				"bad-request",
+				"timeout-or-duplicate"
+			]
+		}`
+		response := &http.Response{
+			Body:       io.NopCloser(strings.NewReader(respBody)),
+			StatusCode: http.StatusOK,
 		}
+
+		httpClientMock.
+			On("Do", mock.AnythingOfType("*http.Request")).
+			Run(func(args mock.Arguments) {
+				req, ok := args.Get(0).(*http.Request)
+				require.True(t, ok)
+
+				assert.Equal(t, verifyTokenURL, req.URL.String())
+				assert.Equal(t, http.MethodPost, req.Method)
+				assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
+
+				reqBody, err := io.ReadAll(req.Body)
+				require.NoError(t, err)
+				defer req.Body.Close()
+
+				assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
+			}).
+			Return(response, nil).
+			Once()
 
 		isValid, err := grv.IsTokenValid(ctx, token)
 
@@ -117,56 +126,61 @@ func Test_GoogleReCAPTCHAValidator(t *testing.T) {
 	})
 
 	t.Run("returns whether the token is invalid or not", func(t *testing.T) {
-		token := "token"
-
-		// Token invalid
-		httpClientMock.mockDo = func(req *http.Request) (*http.Response, error) {
-			assert.Equal(t, verifyTokenURL, req.URL.String())
-			assert.Equal(t, http.MethodPost, req.Method)
-			assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
-
-			reqBody, err := io.ReadAll(req.Body)
-			require.NoError(t, err)
-			defer req.Body.Close()
-
-			assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
-
-			respBody := `{"success": false}`
-
-			return &http.Response{
-				Body:       io.NopCloser(strings.NewReader(respBody)),
-				StatusCode: http.StatusOK,
-			}, nil
+		response1 := &http.Response{
+			Body:       io.NopCloser(strings.NewReader(`{"success": false}`)),
+			StatusCode: http.StatusOK,
 		}
+
+		httpClientMock.
+			On("Do", mock.AnythingOfType("*http.Request")).
+			Run(func(args mock.Arguments) {
+				req, ok := args.Get(0).(*http.Request)
+				require.True(t, ok)
+
+				assert.Equal(t, verifyTokenURL, req.URL.String())
+				assert.Equal(t, http.MethodPost, req.Method)
+				assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
+
+				reqBody, err := io.ReadAll(req.Body)
+				require.NoError(t, err)
+				defer req.Body.Close()
+
+				assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
+			}).
+			Return(response1, nil).
+			Once()
 
 		isValid, err := grv.IsTokenValid(ctx, token)
-
-		assert.False(t, isValid)
 		assert.NoError(t, err)
+		assert.False(t, isValid)
 
 		// Token is valid
-		httpClientMock.mockDo = func(req *http.Request) (*http.Response, error) {
-			assert.Equal(t, verifyTokenURL, req.URL.String())
-			assert.Equal(t, http.MethodPost, req.Method)
-			assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
-
-			reqBody, rErr := io.ReadAll(req.Body)
-			require.NoError(t, rErr)
-			defer req.Body.Close()
-
-			assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
-
-			respBody := `{"success": true}`
-
-			return &http.Response{
-				Body:       io.NopCloser(strings.NewReader(respBody)),
-				StatusCode: http.StatusOK,
-			}, nil
+		response2 := &http.Response{
+			Body:       io.NopCloser(strings.NewReader(`{"success": true}`)),
+			StatusCode: http.StatusOK,
 		}
 
-		isValid, err = grv.IsTokenValid(ctx, token)
+		httpClientMock.
+			On("Do", mock.AnythingOfType("*http.Request")).
+			Run(func(args mock.Arguments) {
+				req, ok := args.Get(0).(*http.Request)
+				require.True(t, ok)
 
-		assert.True(t, isValid)
+				assert.Equal(t, verifyTokenURL, req.URL.String())
+				assert.Equal(t, http.MethodPost, req.Method)
+				assert.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
+
+				reqBody, rErr := io.ReadAll(req.Body)
+				require.NoError(t, rErr)
+				defer req.Body.Close()
+
+				assert.Equal(t, fmt.Sprintf(`secret=%s&response=%s`, siteSecretKey, token), string(reqBody))
+			}).
+			Return(response2, nil).
+			Once()
+
+		isValid, err = grv.IsTokenValid(ctx, token)
 		assert.NoError(t, err)
+		assert.True(t, isValid)
 	})
 }
