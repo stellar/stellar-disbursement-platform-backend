3c9bd4711129753599b91d882fc65c6163c00c61
Marcelo Salloum <marcelo@stellar.org>
2024-04-30 13:25:16 -0300

    Polish script.

--- scripts/exclude_from_coverage.sh
+++ scripts/exclude_from_coverage.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
-while IFS= read -r p || [ -n "$p" ]; do
-  exp=".*${p}.*"
-  grep -v "$exp" c.out > c.out.tmp
-done << EOF # list of terms we want to exclude
-mock
-tss_payments_loadtest.go
-EOF
 
-mv c.out.tmp c.out
+exclude_terms() {
+    local terms="$1"
+    local infile="$2"
+    local tmpfile="${infile}.tmp"
+
+    while IFS= read -r term || [ -n "$term" ]; do
+        local exp=".*${term}.*"
+        grep -v "$exp" "$infile" > "$tmpfile"
+        mv "$tmpfile" "$infile"
+    done << EOF
+$terms
+EOF
+}
+
+# Usage
+exclude_terms "mock" "c.out"
+exclude_terms "tss_payments_loadtest.go" "c.out"
