107cf1107d1a9a7c7eb4c4b6dadade3d16124f43
Reece Markowsky <reece@stellar.org>
2024-05-08 12:31:27 -0700

    do not crash on startup in single tenant mode without default tenant

--- internal/serve/middleware/middleware.go
+++ internal/serve/middleware/middleware.go
@@ -294,10 +294,19 @@ func ResolveTenantFromRequestMiddleware(tenantManager tenant.ManagerInterface, s
 			if singleTenantMode {
 				var err error
 				currentTenant, err = tenantManager.GetDefault(ctx)
+				
 				if err != nil {
 					switch {
 					case errors.Is(err, tenant.ErrTenantDoesNotExist):
-						httperror.InternalError(ctx, "No default Tenant configured", err, nil).Render(rw)
+						// Log the error and allow the request to continue without a tenant.
+						log.Ctx(ctx).Warnf(
+							"No default tenant configured: %v. " +
+							"Use POST /tenant to create a tenant, " +
+							"and POST /default-tenant to set the default tenant.",
+							err,
+						)
+						next.ServeHTTP(rw, req)
+						return
 					case errors.Is(err, tenant.ErrTooManyDefaultTenants):
 						httperror.InternalError(ctx, "Too many default tenants configured", err, nil).Render(rw)
 					default:
