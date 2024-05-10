f52ffe3be7be8f1719d83eca0651f8548f8606c3
Marcelo Salloum <marcelo@stellar.org>
2024-05-10 12:28:07 -0300

    Add new account types

--- pkg/schema/account_type.go
+++ pkg/schema/account_type.go
@@ -5,6 +5,8 @@ import "slices"
 type AccountType string
 
 const (
+	HostStellarEnv                    AccountType = "HOST.STELLAR.ENV"
+	ChannelAccountStellarDB           AccountType = "CHANNEL_ACCOUNT.STELLAR.DB"
 	DistributionAccountStellarEnv     AccountType = "DISTRIBUTION_ACCOUNT.STELLAR.ENV"      // was "ENV_STELLAR"
 	DistributionAccountStellarDBVault AccountType = "DISTRIBUTION_ACCOUNT.STELLAR.DB_VAULT" // was "DB_VAULT_STELLAR"
 	DistributionAccountCircleDBVault  AccountType = "DISTRIBUTION_ACCOUNT.CIRCLE.DB_VAULT"  // was "DB_VAULT_CIRCLE"
