7aae1e1e3f4f53db199a253a847e7d7f29f7dcff
Marcelo Salloum <marcelo@stellar.org>
2024-05-10 12:27:58 -0300

    Move AccountType to a separate file.

--- pkg/schema/transaction_account.go
+++ pkg/schema/transaction_account.go
@@ -1,25 +1,5 @@
 package schema
 
-import (
-	"slices"
-)
-
-type AccountType string
-
-const (
-	DistributionAccountStellarEnv     AccountType = "DISTRIBUTION_ACCOUNT.STELLAR.ENV"      // was "ENV_STELLAR"
-	DistributionAccountStellarDBVault AccountType = "DISTRIBUTION_ACCOUNT.STELLAR.DB_VAULT" // was "DB_VAULT_STELLAR"
-	DistributionAccountCircleDBVault  AccountType = "DISTRIBUTION_ACCOUNT.CIRCLE.DB_VAULT"  // was "DB_VAULT_CIRCLE"
-)
-
-func (t AccountType) IsStellar() bool {
-	return slices.Contains([]AccountType{DistributionAccountStellarEnv, DistributionAccountStellarDBVault}, t)
-}
-
-func (t AccountType) IsCircle() bool {
-	return slices.Contains([]AccountType{DistributionAccountCircleDBVault}, t)
-}
-
 type AccountStatus string
 
 const (
