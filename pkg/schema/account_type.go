f036bd84f3448296acac10771a4c40a8e6c312f4
Marcelo Salloum <marcelo@stellar.org>
2024-05-10 12:29:11 -0300

    Add the Role, StorageMethod and Platform abstractions to make the TransactionAccount easier to use throughout the code.

--- pkg/schema/account_type.go
+++ pkg/schema/account_type.go
@@ -1,7 +1,5 @@
 package schema
 
-import "slices"
-
 type AccountType string
 
 const (
@@ -13,9 +11,71 @@ const (
 )
 
 func (t AccountType) IsStellar() bool {
-	return slices.Contains([]AccountType{DistributionAccountStellarEnv, DistributionAccountStellarDBVault}, t)
+	return t.Platform() == StellarPlatform
 }
 
 func (t AccountType) IsCircle() bool {
-	return slices.Contains([]AccountType{DistributionAccountCircleDBVault}, t)
+	return t.Platform() == CirclePlatform
+}
+
+// Role represents the role of an account in the system.
+type Role string
+
+const (
+	HostRole                Role = "HOST"
+	ChannelAccountRole      Role = "CHANNEL_ACCOUNT"
+	DistributionAccountRole Role = "DISTRIBUTION_ACCOUNT"
+)
+
+var accRoleMap = map[AccountType]Role{
+	HostStellarEnv:                    HostRole,
+	ChannelAccountStellarDB:           ChannelAccountRole,
+	DistributionAccountStellarEnv:     DistributionAccountRole,
+	DistributionAccountStellarDBVault: DistributionAccountRole,
+	DistributionAccountCircleDBVault:  DistributionAccountRole,
+}
+
+func (t AccountType) Role() Role {
+	return accRoleMap[t]
+}
+
+// StorageMethod represents the method used to store the account secret.
+type StorageMethod string
+
+const (
+	EnvStorageMethod     StorageMethod = "ENV"
+	DBStorageMethod      StorageMethod = "DB"
+	DBVaultStorageMethod StorageMethod = "DB_VAULT"
+)
+
+var accStorageMethodMap = map[AccountType]StorageMethod{
+	HostStellarEnv:                    EnvStorageMethod,
+	ChannelAccountStellarDB:           DBStorageMethod,
+	DistributionAccountStellarEnv:     DBVaultStorageMethod,
+	DistributionAccountStellarDBVault: DBVaultStorageMethod,
+	DistributionAccountCircleDBVault:  DBVaultStorageMethod,
+}
+
+func (t AccountType) StorageMethod() StorageMethod {
+	return accStorageMethodMap[t]
+}
+
+// Platform represents the platform where the account is used.
+type Platform string
+
+const (
+	StellarPlatform Platform = "STELLAR"
+	CirclePlatform  Platform = "CIRCLE"
+)
+
+var accPlatformMap = map[AccountType]Platform{
+	HostStellarEnv:                    StellarPlatform,
+	ChannelAccountStellarDB:           StellarPlatform,
+	DistributionAccountStellarEnv:     StellarPlatform,
+	DistributionAccountStellarDBVault: StellarPlatform,
+	DistributionAccountCircleDBVault:  CirclePlatform,
+}
+
+func (t AccountType) Platform() Platform {
+	return accPlatformMap[t]
 }
