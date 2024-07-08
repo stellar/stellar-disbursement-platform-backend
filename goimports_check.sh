c75e8bac1625a4a5527d468acbdf6bbad25f8870
Marcelo Salloum <marcelo@stellar.org>
2024-07-08 16:17:09 -0700

    Make script work the same in local and GH Actions.

--- goimports_check.sh
+++ goimports_check.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
+
+# Check if the script is running in GitHub Actions
+if [ -n "$GITHUB_ACTIONS" ]; then
+  ERROR_COLOR="::error::"
+  SUCCESS_COLOR="::debug::"
+else
+  ERROR_COLOR='\033[0;31m'
+  SUCCESS_COLOR='\033[0;32m'
+fi
+
 # Find all .go files excluding paths containing 'mock' and run goimports
 non_compliant_files=$(find . -type f -name "*.go" ! -path "*mock*" | xargs goimports -local "github.com/stellar/stellar-disbursement-platform-backend" -l)
 
 if [ -n "$non_compliant_files" ]; then
-  printf "::error::The following files are not compliant with goimports:\n"
-  printf "::error::%s\n" "$non_compliant_files"
+  echo -e "${ERROR_COLOR}The following files are not compliant with goimports:\n"
+  echo -e "${ERROR_COLOR}${non_compliant_files}\n"
   exit 1
 else
-  printf "::notice::All files are compliant with goimports.\n"
+  echo -e "${SUCCESS_COLOR}All files are compliant with goimports.\n"
   exit 0
 fi
