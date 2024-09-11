c7e340e0c869faf721bf989b878c2091320054ac
Marcelo Salloum <marcelo@stellar.org>
2024-09-11 16:48:41 -0700

    Add more helpers and tests to the sorobanRPC package.

--- internal/transactionsubmission/services/sorobanrpc/client.go
+++ internal/transactionsubmission/services/sorobanrpc/client.go
@@ -3,9 +3,14 @@ package sorobanrpc
 import (
 	"bytes"
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/stellar/go/xdr"
+
+	"github.com/stellar/stellar-disbursement-platform-backend/internal/utils"
 )
 
 // Authenticator defines an interface for injecting authentication into a request.
@@ -41,10 +46,10 @@ func NewClient(endpoint string, auth Authenticator) *Client {
 
 // RPCRequest represents the JSON-RPC request structure.
 type RPCRequest struct {
-	JSONRPC string        `json:"jsonrpc"`
-	Method  string        `json:"method"`
-	Params  []interface{} `json:"params,omitempty"`
-	ID      int           `json:"id"`
+	JSONRPC string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params,omitempty"`
+	ID      int         `json:"id"`
 }
 
 // RPCResponse represents the JSON-RPC response structure.
@@ -73,7 +78,7 @@ const (
 )
 
 // Call sends a JSON-RPC request to the Soroban RPC server, using variadic params.
-func (c *Client) Call(ctx context.Context, method string, id int, params ...interface{}) (*RPCResponse, error) {
+func (c *Client) Call(ctx context.Context, id int, method string, params interface{}) (*RPCResponse, error) {
 	// Create the request object
 	req := RPCRequest{
 		JSONRPC: "2.0",
@@ -123,4 +128,150 @@ func (c *Client) Call(ctx context.Context, method string, id int, params ...inte
 	}
 
 	return &rpcResp, nil
+}
+
+//////// Get Health
+
+type HealthStatus string
+
+const HealthStatusHealthy HealthStatus = "healthy"
+
+// {"status":"healthy","latestLedger":1513174,"oldestLedger":1495895,"ledgerRetentionWindow":17280}
+type GetHealthResult struct {
+	Status                HealthStatus `json:"status"`
+	OldestLedger          int          `json:"oldestLedger"`
+	LatestLedger          int          `json:"latestLedger"`
+	LedgerRetentionWindow int          `json:"ledgerRetentionWindow"`
+}
+
+type GetHealthResponse struct {
+	RPCResponse
+	Result GetHealthResult `json:"result,omitempty"`
+}
+
+func (c *Client) GetHealth(ctx context.Context, id int) (*GetHealthResponse, error) {
+	rpcResp, err := c.Call(ctx, id, "getHealth", nil)
+	if err != nil {
+		return nil, fmt.Errorf("calling RPC server with getHealth: %w", err)
+	}
+
+	getHealthResp, err := utils.ConvertType[*RPCResponse, *GetHealthResponse](rpcResp)
+	if err != nil {
+		return nil, fmt.Errorf("converting RPC response to GetHealthResponse: %w", err)
+	}
+
+	return getHealthResp, nil
+}
+
+//////// Get Transaction
+
+type TransactionStatus string
+
+const (
+	TransactionStatusSuccess  TransactionStatus = "SUCCESS"
+	TransactionStatusFailed   TransactionStatus = "FAILED"
+	TransactionStatusNotFound TransactionStatus = "NOT_FOUND"
+)
+
+type GetTransactionResult struct {
+	Status                TransactionStatus `json:"status"`
+	LatestLedger          int               `json:"latestLedger"`
+	LatestLedgerCloseTime string            `json:"latestLedgerCloseTime"`
+	OldestLedger          int               `json:"oldestLedger"`
+	OldestLedgerCloseTime string            `json:"oldestLedgerCloseTime"`
+	ApplicationOrder      int               `json:"applicationOrder,omitempty"`
+	EnvelopeXdr           string            `json:"envelopeXdr,omitempty"`
+	ResultXdr             string            `json:"resultXdr,omitempty"`
+	ResultMetaXdr         string            `json:"resultMetaXdr,omitempty"`
+	Ledger                int               `json:"ledger,omitempty"`
+	CreatedAt             string            `json:"createdAt,omitempty"`
+}
+
+type GetTransactionResponse struct {
+	RPCResponse
+	Result GetTransactionResult `json:"result,omitempty"`
+}
+
+func (c *Client) GetTransaction(ctx context.Context, id int, hash string) (*GetTransactionResponse, error) {
+	rpcResp, err := c.Call(ctx, id, "getTransaction", map[string]string{"hash": hash})
+	if err != nil {
+		return nil, fmt.Errorf("calling RPC server with getTransaction: %w", err)
+	}
+
+	getTransactionResp, err := utils.ConvertType[*RPCResponse, *GetTransactionResponse](rpcResp)
+	if err != nil {
+		return nil, fmt.Errorf("converting RPC response to GetTransactionResponse: %w", err)
+	}
+
+	return getTransactionResp, nil
+}
+
+//////// Send Transaction
+
+type SendTransactionStatus string
+
+const (
+	SendTransactionStatusPending       SendTransactionStatus = "PENDING"
+	SendTransactionStatusDuplicate     SendTransactionStatus = "DUPLICATE"
+	SendTransactionStatusTryAgainLater SendTransactionStatus = "TRY_AGAIN_LATER"
+	SendTransactionStatusError         SendTransactionStatus = "ERROR"
+)
+
+type ErrorResultXDR xdr.TransactionResult
+
+// UnmarshalJSON implements the json.Unmarshaler interface for ErrorResultXDR
+func (e *ErrorResultXDR) UnmarshalJSON(data []byte) error {
+	// Step 1: Parse the JSON input to get the base64-encoded XDR string
+	var base64Xdr string
+	if err := json.Unmarshal(data, &base64Xdr); err != nil {
+		return fmt.Errorf("error unmarshalling JSON into base64 string: %w", err)
+	}
+
+	// Step 2: Decode the base64-encoded XDR string
+	rawXdr, err := base64.StdEncoding.DecodeString(base64Xdr)
+	if err != nil {
+		return fmt.Errorf("error decoding base64 XDR string: %w", err)
+	}
+
+	// Step 3: Unmarshal the raw XDR bytes into an xdr.TransactionResult object
+	var txResult xdr.TransactionResult
+	err = xdr.SafeUnmarshal(rawXdr, &txResult)
+	if err != nil {
+		return fmt.Errorf("error unmarshalling XDR to TransactionResult: %w", err)
+	}
+
+	// Step 4: Assign the unmarshalled TransactionResult to the receiver
+	*e = ErrorResultXDR(txResult)
+
+	return nil
+}
+
+type SendTransactionResult struct {
+	Hash                  string                `json:"hash"`
+	Status                SendTransactionStatus `json:"status"`
+	LatestLedger          int                   `json:"latestLedger"`
+	LatestLedgerCloseTime string                `json:"latestLedgerCloseTime"`
+	DiagnosticEventsXDR   string                `json:"diagnosticEventsXdr,omitempty"`
+	// TODO: maybe use xdr.TransactionResult
+	ErrorResultXDR ErrorResultXDR `json:"errorResultXdr,omitempty"`
+	// ErrorResultXDR string `json:"errorResultXdr,omitempty"`
+}
+
+type SendTransactionResponse struct {
+	RPCResponse
+	Result SendTransactionResult `json:"result,omitempty"`
+}
+
+func (c *Client) SendTransaction(ctx context.Context, id int, txXDR string) (*SendTransactionResponse, error) {
+	rpcResp, err := c.Call(ctx, id, "sendTransaction", map[string]string{"transaction": txXDR})
+	if err != nil {
+		return nil, fmt.Errorf("calling RPC server with sendTransaction: %w", err)
+	}
+
+	sendTransactionResp, err := utils.ConvertType[*RPCResponse, *SendTransactionResponse](rpcResp)
+	if err != nil {
+		return nil, fmt.Errorf("converting RPC response to SendTransactionResponse: %w", err)
+	}
+
+	return sendTransactionResp, nil
 }
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
 	"github.com/stellar/go/support/log"
+	"github.com/stellar/go/txnbuild"
 
-	"github.com/stellar/stellar-disbursement-platform-backend/cmd"
+	"github.com/stellar/stellar-disbursement-platform-backend/internal/serve/httpclient"
+	"github.com/stellar/stellar-disbursement-platform-backend/internal/transactionsubmission/services/sorobanrpc"
+	tssUtils "github.com/stellar/stellar-disbursement-platform-backend/internal/transactionsubmission/utils"
 )
 
 // Version is the official version of this application. Whenever it's changed
@@ -19,18 +25,159 @@ const Version = "2.1.0"
 // go build -ldflags "-X main.GitCommit=$GIT_COMMIT"
 var GitCommit string
 
+const (
+	AccountSecret     = "SAARF2ZWAHZJMKA6LXIFVNIHUBEUTMKV5NWCCUZV6ORPKLUK6RSOYZ4D"
+	TestTxHash        = "0d692b8dbb41bc9f2efce7a9be3d0502fb2348ab28fd4b2574bd7fe1a488cb36"
+	NetworkPassphrase = network.TestNetworkPassphrase
+	HorizonURL        = "https://horizon-testnet.stellar.org"
+	SorobanURL        = "https://soroban-testnet.stellar.org"
+)
+
+type SorobanService struct {
+	rpcClient         *sorobanrpc.Client
+	sourceAccKP       *keypair.Full
+	networkPassphrase string
+	horizonClient     horizonclient.ClientInterface
+}
+
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Debug("No .env file found")
 	}
 
 	preConfigureLogger()
+	ctx := context.Background()
 
-	rootCmd := cmd.SetupCLI(Version, GitCommit)
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	s := NewSorobanService()
+
+	fmt.Println("\n====================================")
+	s.printHealth(ctx)
+	fmt.Println("\n====================================")
+	s.printGetTransaction(ctx, TestTxHash)
+	fmt.Println("\n====================================")
+	s.printSendTransaction(ctx)
+	fmt.Println("\n====================================")
+}
+
+func NewSorobanService() SorobanService {
+	sourceAccKP := keypair.MustParseFull(AccountSecret)
+	rpcClient := sorobanrpc.NewClient(SorobanURL, nil)
+	horizonClient := &horizonclient.Client{
+		HorizonURL: HorizonURL,
+		HTTP:       httpclient.DefaultClient(),
 	}
+	s := SorobanService{
+		rpcClient:         rpcClient,
+		sourceAccKP:       sourceAccKP,
+		networkPassphrase: NetworkPassphrase,
+		horizonClient:     horizonClient,
+	}
+	return s
+}
+
+func (s *SorobanService) printHealth(ctx context.Context) {
+	resp, err := s.rpcClient.Call(ctx, 1, "getHealth", nil)
+	if err != nil {
+		log.Ctx(ctx).Panicf("Error calling RPC with getHealth: %v", err)
+	}
+	log.Ctx(ctx).Infof("RPC Call(getHealth): %s", resp.Result)
+
+	resp2, err := s.rpcClient.GetHealth(ctx, 1)
+	if err != nil {
+		log.Ctx(ctx).Panicf("Error calling RPC.GetHealth: %v", err)
+	}
+	log.Ctx(ctx).Infof("RPC GetHealth: %+v", resp2.Result)
+}
+
+func (s *SorobanService) printGetTransaction(ctx context.Context, hash string) {
+	resp1, err := s.rpcClient.Call(ctx, 1, "getTransaction", map[string]string{"hash": hash})
+	if err != nil {
+		log.Ctx(ctx).Panicf("Error calling RPC with getTransaction: %v", err)
+	}
+	log.Ctx(ctx).Infof("RPC Call(getTransaction): %s", resp1.Result)
+
+	resp2, err := s.rpcClient.GetTransaction(ctx, 1, hash)
+	if err != nil {
+		log.Ctx(ctx).Panicf("Error calling RPC.GetTransaction: %v", err)
+	}
+	log.Ctx(ctx).Infof("RPC GetTransaction: %+v", resp2.Result)
+}
+
+func (s *SorobanService) printSendTransaction(ctx context.Context) {
+	tx1, err := s.buildAndSignNativePaymentTx(ctx)
+	if err != nil {
+		log.Ctx(ctx).Panicf("Error building&signing transaction: %v", err)
+	}
+	resp1, err := s.rpcClient.Call(ctx, 1, "sendTransaction", map[string]string{"transaction": tx1.XDR})
+	if err != nil {
+		log.Ctx(ctx).Panicf("Error calling RPC with getTransaction: %v", err)
+	}
+	log.Ctx(ctx).Infof("RPC Call(sendTransaction, hash=%s): %s", tx1.Hash, resp1.Result)
+
+	tx2, err := s.buildAndSignNativePaymentTx(ctx)
+	if err != nil {
+		log.Ctx(ctx).Panicf("Error building&signing transaction: %v", err)
+	}
+	resp2, err := s.rpcClient.SendTransaction(ctx, 1, tx2.XDR)
+	if err != nil {
+		log.Ctx(ctx).Panicf("Error calling RPC.SendTransaction: %v", err)
+	}
+	log.Ctx(ctx).Infof("RPC SendTransaction (hash=%s): %+v", tx2.Hash, resp2.Result)
+}
+
+type TxWithMetadata struct {
+	XDR  string
+	Hash string
+	tx   txnbuild.Transaction
+}
+
+func (s *SorobanService) buildAndSignNativePaymentTx(_ context.Context) (*TxWithMetadata, error) {
+	hAcc, err := s.horizonClient.AccountDetail(horizonclient.AccountRequest{AccountID: s.sourceAccKP.Address()})
+	if err != nil {
+		return nil, tssUtils.NewHorizonErrorWrapper(err)
+	}
+
+	tx, err := txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &hAcc,
+			IncrementSequenceNum: true,
+			BaseFee:              txnbuild.MinBaseFee * 100,
+			Preconditions: txnbuild.Preconditions{
+				TimeBounds: txnbuild.NewTimeout(60),
+			},
+			Operations: []txnbuild.Operation{
+				&txnbuild.Payment{
+					Destination: s.sourceAccKP.Address(),
+					Amount:      "1",
+					Asset:       txnbuild.NativeAsset{},
+				},
+			},
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("creating transaction: %w", err)
+	}
+
+	tx, err = tx.Sign(s.networkPassphrase, s.sourceAccKP)
+	if err != nil {
+		return nil, fmt.Errorf("signing transaction: %w", err)
+	}
+
+	txHash, err := tx.HashHex(s.networkPassphrase)
+	if err != nil {
+		return nil, fmt.Errorf("hashing transaction: %w", err)
+	}
+
+	txXDR, err := tx.Base64()
+	if err != nil {
+		return nil, fmt.Errorf("converting transaction to base64: %w", err)
+	}
+
+	return &TxWithMetadata{
+		XDR:  txXDR,
+		Hash: txHash,
+		tx:   *tx,
+	}, nil
 }
 
 // preConfigureLogger will set the log level to Trace, so logs works from the
