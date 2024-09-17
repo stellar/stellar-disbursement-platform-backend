d2b486a666cfdcf6f701f2dea3219a91be04f3ee
Marwen Abid <marwen.abid@stellar.org>
2024-09-16 18:55:27 -0700

    SDP-1343 HTML Injection Vulnerability

--- internal/htmltemplate/htmltemplate_test.go
+++ internal/htmltemplate/htmltemplate_test.go
@@ -14,7 +14,7 @@ func Test_ExecuteHTMLTemplate(t *testing.T) {
 	var inputData interface{}
 	templateStr, err := ExecuteHTMLTemplate("non-existing-file.html", inputData)
 	require.Empty(t, templateStr)
-	require.EqualError(t, err, `executing html template: template: no template "non-existing-file.html" associated with template "empty_body.tmpl"`)
+	require.EqualError(t, err, `executing html template: html/template: "non-existing-file.html" is undefined`)
 
 	// handle invalid struct body
 	inputData = struct {
@@ -65,6 +65,24 @@ func Test_ExecuteHTMLTemplateForInvitationMessage(t *testing.T) {
 	assert.Contains(t, content, "as a developer.")
 	assert.Contains(t, content, forgotPasswordLink)
 	assert.Contains(t, content, "Organization Name")
+}
+
+func Test_ExecuteHTMLTemplateForInvitationMessage_HTMLInjectionAttack(t *testing.T) {
+	forgotPasswordLink := "https://sdp.com/forgot-password"
+
+	data := InvitationMessageTemplate{
+		FirstName:          "First",
+		Role:               "developer",
+		ForgotPasswordLink: forgotPasswordLink,
+		OrganizationName:   "<a href='evil.com'>Redeem funds</a>",
+	}
+	content, err := ExecuteHTMLTemplateForInvitationMessage(data)
+	require.NoError(t, err)
+
+	assert.Contains(t, content, "Hello, First!")
+	assert.Contains(t, content, "as a developer.")
+	assert.Contains(t, content, forgotPasswordLink)
+	assert.Contains(t, content, "&lt;a href=&#39;evil.com&#39;&gt;Redeem funds&lt;/a&gt;")
 }
 
 func Test_ExecuteHTMLTemplateForForgotPasswordMessage(t *testing.T) {
