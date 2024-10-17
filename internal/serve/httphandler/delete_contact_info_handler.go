f53b7281b964d16cb44cad1d2e613c0e05c557a0
Marcelo Salloum <marcelo@stellar.org>
2024-10-17 14:17:44 -0700

    Rename DeletePhoneNumberHandler to DeleteContactInfoHandler and updated the route from `DELETE /phone-number/{phone_number}` to `DELETE /contact-info/{contact_info}`.

--- internal/serve/httphandler/delete_contact_info_handler.go
+++ internal/serve/httphandler/delete_contact_info_handler.go
@@ -3,6 +3,7 @@ package httphandler
 import (
 	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/stellar/go/network"
@@ -14,27 +15,37 @@ import (
 	"github.com/stellar/stellar-disbursement-platform-backend/internal/utils"
 )
 
-type DeletePhoneNumberHandler struct {
+type DeleteContactInfoHandler struct {
 	NetworkPassphrase string
 	Models            *data.Models
 }
 
-func (d DeletePhoneNumberHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (d DeleteContactInfoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	if d.NetworkPassphrase != network.TestNetworkPassphrase {
 		httperror.NotFound("", nil, nil).Render(w)
 		return
 	}
 
-	phoneNumber := chi.URLParam(r, "phone_number")
-	if err := utils.ValidatePhoneNumber(phoneNumber); err != nil {
-		extras := map[string]interface{}{"phone_number": "invalid phone number"}
+	contactInfo := strings.TrimSpace(chi.URLParam(r, "contact_info"))
+	extras := map[string]interface{}{}
+	if contactInfo == "" {
+		extras["contact_info"] = "contact_info is required"
+	} else {
+		phoneNumberErr := utils.ValidatePhoneNumber(contactInfo)
+		emailErr := utils.ValidateEmail(contactInfo)
+
+		if phoneNumberErr != nil && emailErr != nil {
+			extras["contact_info"] = "not a valid phone number or email"
+		}
+	}
+	if len(extras) > 0 {
 		httperror.BadRequest("", nil, extras).Render(w)
 		return
 	}
 
-	log.Ctx(ctx).Warnf("Deleting user with phone number %s", utils.TruncateString(phoneNumber, 3))
-	err := d.Models.Receiver.DeleteByContactInfo(ctx, d.Models.DBConnectionPool, phoneNumber)
+	log.Ctx(ctx).Warnf("Deleting user with phone number %s", utils.TruncateString(contactInfo, 3))
+	err := d.Models.Receiver.DeleteByContactInfo(ctx, d.Models.DBConnectionPool, contactInfo)
 	if err != nil {
 		if errors.Is(err, data.ErrRecordNotFound) {
 			httperror.NotFound("", err, nil).Render(w)
--- internal/serve/httphandler/delete_contact_info_handler_test.go
+++ internal/serve/httphandler/delete_contact_info_handler_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/stellar/stellar-disbursement-platform-backend/internal/data"
 )
 
-func Test_DeletePhoneNumberHandler(t *testing.T) {
+func Test_DeleteContactInfoHandler(t *testing.T) {
 	dbt := dbtest.Open(t)
 	defer dbt.Close()
 	dbConnectionPool, err := db.OpenDBConnectionPool(dbt.DSN)
@@ -26,15 +26,15 @@ func Test_DeletePhoneNumberHandler(t *testing.T) {
 	ctx := context.Background()
 	models, err := data.NewModels(dbConnectionPool)
 	require.NoError(t, err)
-	receiver := data.CreateReceiverFixture(t, ctx, dbConnectionPool, &data.Receiver{PhoneNumber: "+14152222222"})
+	receiverPhoneNumber := data.CreateReceiverFixture(t, ctx, dbConnectionPool, &data.Receiver{PhoneNumber: "+14152222222"})
 
 	t.Run("return 404 if network passphrase is not testnet", func(t *testing.T) {
-		h := DeletePhoneNumberHandler{NetworkPassphrase: network.PublicNetworkPassphrase, Models: models}
+		h := DeleteContactInfoHandler{NetworkPassphrase: network.PublicNetworkPassphrase, Models: models}
 		r := chi.NewRouter()
-		r.Delete("/wallet-registration/phone-number/{phone_number}", h.ServeHTTP)
+		r.Delete("/wallet-registration/contact-info/{contact_info}", h.ServeHTTP)
 
 		// test
-		req, err := http.NewRequest("DELETE", "/wallet-registration/phone-number/"+receiver.PhoneNumber, nil)
+		req, err := http.NewRequest("DELETE", "/wallet-registration/contact-info/"+receiverPhoneNumber.PhoneNumber, nil)
 		require.NoError(t, err)
 		rr := httptest.NewRecorder()
 		r.ServeHTTP(rr, req)
@@ -46,12 +46,12 @@ func Test_DeletePhoneNumberHandler(t *testing.T) {
 	})
 
 	t.Run("return 400 if network passphrase is testnet but phone number is invalid", func(t *testing.T) {
-		h := DeletePhoneNumberHandler{NetworkPassphrase: network.TestNetworkPassphrase, Models: models}
+		h := DeleteContactInfoHandler{NetworkPassphrase: network.TestNetworkPassphrase, Models: models}
 		r := chi.NewRouter()
-		r.Delete("/wallet-registration/phone-number/{phone_number}", h.ServeHTTP)
+		r.Delete("/wallet-registration/contact-info/{contact_info}", h.ServeHTTP)
 
 		// test
-		req, err := http.NewRequest("DELETE", "/wallet-registration/phone-number/foobar", nil)
+		req, err := http.NewRequest("DELETE", "/wallet-registration/contact-info/foobar", nil)
 		require.NoError(t, err)
 		rr := httptest.NewRecorder()
 		r.ServeHTTP(rr, req)
@@ -61,19 +61,19 @@ func Test_DeletePhoneNumberHandler(t *testing.T) {
 		wantJson := `{
 			"error": "The request was invalid in some way.",
 			"extras": {
-				"phone_number": "invalid phone number"
+				"contact_info": "not a valid phone number or email"
 			}
 		}`
 		assert.JSONEq(t, wantJson, rr.Body.String())
 	})
 
 	t.Run("return 404 if network passphrase is testnet but phone number does not exist", func(t *testing.T) {
-		h := DeletePhoneNumberHandler{NetworkPassphrase: network.TestNetworkPassphrase, Models: models}
+		h := DeleteContactInfoHandler{NetworkPassphrase: network.TestNetworkPassphrase, Models: models}
 		r := chi.NewRouter()
-		r.Delete("/wallet-registration/phone-number/{phone_number}", h.ServeHTTP)
+		r.Delete("/wallet-registration/contact-info/{contact_info}", h.ServeHTTP)
 
 		// test
-		req, err := http.NewRequest("DELETE", "/wallet-registration/phone-number/+14153333333", nil)
+		req, err := http.NewRequest("DELETE", "/wallet-registration/contact-info/+14153333333", nil)
 		require.NoError(t, err)
 		rr := httptest.NewRecorder()
 		r.ServeHTTP(rr, req)
@@ -84,13 +84,13 @@ func Test_DeletePhoneNumberHandler(t *testing.T) {
 		assert.JSONEq(t, wantJson, rr.Body.String())
 	})
 
-	t.Run("return 204 if network passphrase is testnet and phone nymber exists", func(t *testing.T) {
-		h := DeletePhoneNumberHandler{NetworkPassphrase: network.TestNetworkPassphrase, Models: models}
+	t.Run("return 204 if network passphrase is testnet and phone number exists", func(t *testing.T) {
+		h := DeleteContactInfoHandler{NetworkPassphrase: network.TestNetworkPassphrase, Models: models}
 		r := chi.NewRouter()
-		r.Delete("/wallet-registration/phone-number/{phone_number}", h.ServeHTTP)
+		r.Delete("/wallet-registration/contact-info/{contact_info}", h.ServeHTTP)
 
 		// test
-		req, err := http.NewRequest("DELETE", "/wallet-registration/phone-number/"+receiver.PhoneNumber, nil)
+		req, err := http.NewRequest("DELETE", "/wallet-registration/contact-info/"+receiverPhoneNumber.PhoneNumber, nil)
 		require.NoError(t, err)
 		rr := httptest.NewRecorder()
 		r.ServeHTTP(rr, req)
