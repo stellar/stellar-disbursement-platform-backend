bea46615340c6a680017bc27a216cd117947add7
Marcelo Salloum <marcelo@stellar.org>
2024-10-29 16:15:04 -0700

    Do a small refactor on the LoginHandler in preparation for the API role.

--- internal/serve/httphandler/login_handler.go
+++ internal/serve/httphandler/login_handler.go
@@ -1,6 +1,7 @@
 package httphandler
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
@@ -26,19 +27,6 @@ type LoginRequest struct {
 	ReCAPTCHAToken string `json:"recaptcha_token"`
 }
 
-func (r LoginRequest) validate() *httperror.HTTPError {
-	validator := validators.NewValidator()
-
-	validator.Check(r.Email != "", "email", "email is required")
-	validator.Check(r.Password != "", "password", "password is required")
-
-	if validator.HasErrors() {
-		return httperror.BadRequest("Request invalid", nil, validator.Errors)
-	}
-
-	return nil
-}
-
 type LoginResponse struct {
 	Token string `json:"token"`
 }
@@ -52,9 +40,27 @@ type LoginHandler struct {
 	MFADisabled        bool
 }
 
+func (h LoginHandler) validateRequest(req LoginRequest, headers http.Header) *httperror.HTTPError {
+	lv := validators.NewValidator()
+
+	lv.Check(req.Email != "", "email", "email is required")
+	lv.Check(req.Password != "", "password", "password is required")
+	lv.Check(h.ReCAPTCHADisabled || req.ReCAPTCHAToken != "", "recaptcha_token", "reCAPTCHA token is required")
+
+	deviceID := headers.Get(DeviceIDHeader)
+	lv.Check(h.MFADisabled || deviceID != "", DeviceIDHeader, "Device-ID header is required")
+
+	if lv.HasErrors() {
+		return httperror.BadRequest("", nil, lv.Errors)
+	}
+
+	return nil
+}
+
 func (h LoginHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
 
+	// Step 1: Decode and validate the incoming request
 	var reqBody LoginRequest
 	if err := httpdecode.DecodeJSON(req, &reqBody); err != nil {
 		err = fmt.Errorf("decoding the request body: %w", err)
@@ -62,12 +68,14 @@ func (h LoginHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		httperror.BadRequest("", err, nil).Render(rw)
 		return
 	}
-
-	if err := reqBody.validate(); err != nil {
-		err.Render(rw)
+	if httpErr := h.validateRequest(reqBody, req.Header); httpErr != nil {
+		httpErr.Render(rw)
 		return
 	}
 
+	truncatedEmail := utils.TruncateString(reqBody.Email, 3)
+
+	// Step 2: Run the reCAPTCHA validation if it is enabled
 	if !h.ReCAPTCHADisabled {
 		// validating reCAPTCHA Token
 		isValid, err := h.ReCAPTCHAValidator.IsTokenValid(ctx, reqBody.ReCAPTCHAToken)
@@ -77,74 +85,70 @@ func (h LoginHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		}
 
 		if !isValid {
-			log.Ctx(ctx).Errorf("reCAPTCHA token is invalid for request with email %s", utils.TruncateString(reqBody.Email, 3))
+			log.Ctx(ctx).Errorf("reCAPTCHA token is invalid for request with email %s", truncatedEmail)
 			httperror.BadRequest("reCAPTCHA token invalid", nil, nil).Render(rw)
 			return
 		}
 	}
 
+	// Step 3: Authenticate the user
 	token, err := h.AuthManager.Authenticate(ctx, reqBody.Email, reqBody.Password)
 	if errors.Is(err, auth.ErrInvalidCredentials) {
 		httperror.Unauthorized("", err, map[string]interface{}{"details": "Incorrect email or password"}).Render(rw)
 		return
 	}
 	if err != nil {
-		log.Ctx(ctx).Errorf("authenticating user with email %s: %s", utils.TruncateString(reqBody.Email, 3), err)
+		log.Ctx(ctx).Errorf("authenticating user with email %s: %s", truncatedEmail, err)
 		httperror.InternalError(ctx, "Cannot authenticate user credentials", err, nil).Render(rw)
 		return
 	}
 
 	user, err := h.AuthManager.GetUser(ctx, token)
 	if err != nil {
-		log.Ctx(ctx).Errorf("getting user with email %s: %s", utils.TruncateString(reqBody.Email, 3), err)
+		log.Ctx(ctx).Errorf("getting user with email %s: %s", truncatedEmail, err)
 		httperror.InternalError(ctx, "", err, nil).Render(rw)
 		return
 	}
 
+	// Step 4.A: If MFA is disabled, return the token
 	if h.MFADisabled {
 		log.Ctx(ctx).Infof("[UserLogin] - Logged in user with account ID %s", user.ID)
 		httpjson.RenderStatus(rw, http.StatusOK, LoginResponse{Token: token}, httpjson.JSON)
 		return
 	}
 
-	// 🔒 Handling MFA
+	// Step 4.B: If MFA is enabled, check if the device is remembered
 	deviceID := req.Header.Get(DeviceIDHeader)
-	if deviceID == "" {
-		httperror.BadRequest("Device-ID header is required", nil, nil).Render(rw)
-		return
-	}
-
-	isRemembered, err := h.AuthManager.MFADeviceRemembered(ctx, deviceID, user.ID)
-	if err != nil {
-		log.Ctx(ctx).Errorf("checking if device is remembered for user with email %s: %s", utils.TruncateString(reqBody.Email, 3), err.Error())
+	var isRemembered bool
+	if isRemembered, err = h.AuthManager.MFADeviceRemembered(ctx, deviceID, user.ID); err != nil {
+		err = fmt.Errorf("checking if device is remembered for user with email %s: %w", truncatedEmail, err)
 		httperror.InternalError(ctx, "", err, nil).Render(rw)
 		return
-	}
-
-	if isRemembered {
+	} else if isRemembered {
 		log.Ctx(ctx).Infof("[UserLogin] - Logged in user with account ID %s", user.ID)
 		httpjson.RenderStatus(rw, http.StatusOK, LoginResponse{Token: token}, httpjson.JSON)
 		return
 	}
 
-	// Get the MFA code for the user
+	// Step 4.C: If MFA is enabled and the device is not remembered, send the MFA code
 	code, err := h.AuthManager.GetMFACode(ctx, deviceID, user.ID)
 	if err != nil {
-		log.Ctx(ctx).Errorf("getting MFA code for user with email %s: %s", utils.TruncateString(reqBody.Email, 3), err.Error())
+		log.Ctx(ctx).Errorf("getting MFA code for user with email %s: %s", truncatedEmail, err.Error())
 		httperror.InternalError(ctx, "Cannot get MFA code", err, nil).Render(rw)
 		return
 	}
-
-	if code == "" {
-		log.Ctx(ctx).Errorf("MFA code for user with email %s is empty", utils.TruncateString(reqBody.Email, 3))
-		httperror.InternalError(ctx, "Cannot get MFA code", err, nil).Render(rw)
+	if httpErr := h.sendMFAEmail(ctx, user, code); httpErr != nil {
+		httpErr.Render(rw)
 		return
 	}
 
+	httpjson.RenderStatus(rw, http.StatusOK, map[string]string{"message": "MFA code sent to email. Check your inbox and spam folders."}, httpjson.JSON)
+}
+
+func (h LoginHandler) sendMFAEmail(ctx context.Context, user *auth.User, code string) *httperror.HTTPError {
 	organization, err := h.Models.Organizations.Get(ctx)
 	if err != nil {
-		httperror.InternalError(ctx, "Cannot fetch organization", err, nil).Render(rw)
-		return
+		return httperror.InternalError(ctx, "Cannot fetch organization", err, nil)
 	}
 
 	msgTemplate := htmltemplate.StaffMFAEmailMessageTemplate{
@@ -153,8 +157,7 @@ func (h LoginHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	}
 	msgContent, err := htmltemplate.ExecuteHTMLTemplateForStaffMFAEmailMessage(msgTemplate)
 	if err != nil {
-		httperror.InternalError(ctx, "Cannot execute mfa message template", err, nil).Render(rw)
-		return
+		return httperror.InternalError(ctx, "Cannot execute mfa message template", err, nil)
 	}
 
 	msg := message.Message{
@@ -162,13 +165,10 @@ func (h LoginHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		Title:   mfaMessageTitle,
 		Body:    msgContent,
 	}
-	err = h.MessengerClient.SendMessage(msg)
-	if err != nil {
+	if err = h.MessengerClient.SendMessage(msg); err != nil {
 		err = fmt.Errorf("sending mfa code for email %s: %w", user.Email, err)
-		log.Ctx(ctx).Error(err)
-		httperror.InternalError(ctx, "", err, nil).Render(rw)
-		return
+		return httperror.InternalError(ctx, "", err, nil)
 	}
 
-	httpjson.RenderStatus(rw, http.StatusOK, map[string]string{"message": "MFA code sent to email. Check your inbox and spam folders."}, httpjson.JSON)
+	return nil
 }
