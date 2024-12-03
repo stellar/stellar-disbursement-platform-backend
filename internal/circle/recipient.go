3e10c099c46a7f6be09b8f03887410738bda19a0
Marcelo Salloum <marcelo@stellar.org>
2024-12-03 11:17:06 -0800

    Create recipient schema.

---
+++ internal/circle/recipient.go
@@ -0,0 +1,73 @@
+package circle
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/stellar/go/strkey"
+
+	"github.com/stellar/stellar-disbursement-platform-backend/internal/utils"
+)
+
+type Recipient struct {
+	ID         string            `json:"id"`
+	Chain      string            `json:"chain"`
+	Address    string            `json:"address"`
+	Metadata   RecipientMetadata `json:"metadata"`
+	Status     string            `json:"status"`
+	CreateDate string            `json:"createDate"`
+	UpdateDate string            `json:"updateDate"`
+}
+
+type RecipientMetadata struct {
+	Nickname string `json:"nickname"`
+	Email    string `json:"email"`
+	// BNS stands for Blockchain Name Service (e.g. ENS) domain for the address.
+	BNS string `json:"bns"`
+}
+
+type RecipientRequest struct {
+	IdempotencyKey string            `json:"idempotencyKey"`
+	Address        string            `json:"address"`
+	Chain          string            `json:"chain"`
+	Metadata       RecipientMetadata `json:"metadata"`
+}
+
+func (abr *RecipientRequest) validate() error {
+	if abr.IdempotencyKey == "" {
+		return errors.New("idempotency key must be provided")
+	}
+	if _, err := uuid.Parse(abr.IdempotencyKey); err != nil {
+		return errors.New("idempotency key is not a valid UUID")
+	}
+
+	if abr.Address == "" {
+		return errors.New("address must be provided")
+	}
+	if !strkey.IsValidEd25519PublicKey(abr.Address) {
+		return errors.New("address is not a valid Stellar public key")
+	}
+
+	if abr.Chain != "" && abr.Chain != StellarChainCode {
+		return fmt.Errorf("invalid chain provided %q", abr.Chain)
+	} else if abr.Chain == "" {
+		abr.Chain = StellarChainCode
+	}
+
+	if utils.IsEmpty(abr.Metadata) {
+		return errors.New("metadata must be provided")
+	}
+
+	if abr.Metadata.Nickname == "" {
+		return errors.New("metadata nickname must be provided")
+	}
+
+	if abr.Metadata.Email != "" {
+		if err := utils.ValidateEmail(abr.Metadata.Email); err != nil {
+			return errors.New("metadata email is not a valid email")
+		}
+	}
+
+	return nil
+}
