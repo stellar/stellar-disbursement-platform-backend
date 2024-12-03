580285a395f50dd643596bcd35cfcd6dc4168d73
Marcelo Salloum <marcelo@stellar.org>
2024-12-03 13:46:45 -0800

    Create Payout schema(s).

---
+++ internal/circle/payout.go
@@ -0,0 +1,110 @@
+package circle
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/google/uuid"
+	"github.com/stellar/go/strkey"
+)
+
+type RiskEvaluation struct {
+	Decision string `json:"decision"`
+	Reason   string `json:"reason"`
+}
+
+type Payout struct {
+	ID              string            `json:"id"`
+	Destination     TransferAccount   `json:"destination"`
+	Amount          Balance           `json:"amount"`
+	ToAmount        Balance           `json:"toAmount"`
+	TransactionHash string            `json:"externalRef,omitempty"`
+	CreateDate      string            `json:"createDate"`
+	UpdateDate      string            `json:"updateDate"`
+	SourceWalletID  string            `json:"sourceWalletId"`
+	Fees            Balance           `json:"fees,omitempty"`
+	Status          TransferStatus    `json:"status"`
+	ErrorCode       TransferErrorCode `json:"errorCode,omitempty"`
+	RiskEvaluation  RiskEvaluation    `json:"riskEvaluation,omitempty"`
+}
+
+type ToAmount struct {
+	Currency string `json:"currency"`
+}
+
+// PayoutRequest represents the request to create a new transfer.
+type PayoutRequest struct {
+	IdempotencyKey string          `json:"idempotencyKey"`
+	Source         TransferAccount `json:"source"`
+	Destination    TransferAccount `json:"destination"`
+	Amount         Balance         `json:"amount"`
+	ToAmount       ToAmount        `json:"toAmount"`
+}
+
+func (pr *PayoutRequest) validate() error {
+	if pr.IdempotencyKey == "" {
+		return errors.New("idempotency key must be provided")
+	}
+	if _, err := uuid.Parse(pr.IdempotencyKey); err != nil {
+		return errors.New("idempotency key is not a valid UUID")
+	}
+
+	if pr.Source.Type == "" {
+		return fmt.Errorf("source type must be provided")
+	}
+	if pr.Source.Type != TransferAccountTypeWallet {
+		return fmt.Errorf("source type must be wallet")
+	}
+	if pr.Source.ID == "" {
+		return fmt.Errorf("source ID must be provided for wallet transfers")
+	}
+
+	if pr.Destination.Type != TransferAccountTypeBlockchain {
+		return fmt.Errorf("destination type must be blockchain")
+	}
+	if pr.Destination.Chain != "" && pr.Destination.Chain != StellarChainCode {
+		return fmt.Errorf("invalid destination chain provided %q", pr.Destination.Chain)
+	} else if pr.Destination.Chain == "" {
+		pr.Destination.Chain = StellarChainCode
+	}
+	if pr.Destination.Address == "" {
+		return fmt.Errorf("destination address must be provided")
+	}
+	if !strkey.IsValidEd25519PublicKey(pr.Destination.Address) {
+		return errors.New("destination address is not a valid Stellar public key")
+	}
+
+	if pr.Amount.Currency == "" {
+		return fmt.Errorf("currency must be provided")
+	}
+	if pr.Amount.Amount == "" {
+		return fmt.Errorf("amount must be provided")
+	}
+	if _, err := strconv.ParseFloat(pr.Amount.Amount, 64); err != nil {
+		return fmt.Errorf("amount must be a valid number")
+	}
+
+	if pr.ToAmount.Currency == "" {
+		return fmt.Errorf("toAmount.currency must be provided")
+	}
+
+	return nil
+}
+
+// PayoutResponse represents the response from the Circle APIs.
+type PayoutResponse struct {
+	Data Payout `json:"data"`
+}
+
+// parsePayoutResponse parses the response from the Circle APIs.
+func parsePayoutResponse(resp *http.Response) (*Payout, error) {
+	var payoutResponse PayoutResponse
+	if err := json.NewDecoder(resp.Body).Decode(&payoutResponse); err != nil {
+		return nil, fmt.Errorf("decoding transfer response: %w", err)
+	}
+
+	return &payoutResponse.Data, nil
+}
