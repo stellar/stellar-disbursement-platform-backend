8ff533f5da0a3f416f19608dcebbdaf9b75214cd
Marcelo Salloum <marcelo@stellar.org>
2024-12-09 14:28:37 -0800

    Add indexes to the statuses commonly filtered for.

--- db/migrations/sdp-migrations/2024-12-10.0-circle-payouts-support.sql
+++ db/migrations/sdp-migrations/2024-12-10.0-circle-payouts-support.sql
@@ -1,6 +1,7 @@
 -- prepares the database for Circle payouts by adding the new circle_recipients table, and modifying circle_transfers to be used for payouts.
 
 -- +migrate Up
+-- circle_recipients
 CREATE TYPE circle_recipient_status AS ENUM ('pending', 'active', 'inactive', 'denied');
 
 CREATE TABLE circle_recipients (
@@ -16,20 +17,32 @@ CREATE TABLE circle_recipients (
     CONSTRAINT unique_idempotency_key UNIQUE (idempotency_key)
 );
 
+CREATE TRIGGER refresh_circle_recipient_updated_at BEFORE UPDATE ON circle_recipients FOR EACH ROW EXECUTE PROCEDURE update_at_refresh();
+
+-- circle_transfer_requests
 ALTER TABLE circle_transfer_requests
     ADD COLUMN circle_payout_id VARCHAR(36),
     ADD CONSTRAINT circle_transfer_or_payout CHECK (
         NOT (circle_transfer_id IS NOT NULL AND circle_payout_id IS NOT NULL)
     );
 
--- TRIGGER: updated_at
-CREATE TRIGGER refresh_circle_recipient_updated_at BEFORE UPDATE ON circle_recipients FOR EACH ROW EXECUTE PROCEDURE update_at_refresh();
+-- status indexes
+CREATE INDEX idx_payments_status ON payments(status);
+CREATE INDEX idx_disbursements_status ON disbursements(status);
+CREATE INDEX idx_receiver_wallets_status ON receiver_wallets(status);
 
 -- +migrate Down
+-- status indexes
+DROP INDEX IF EXISTS idx_payments_status;
+DROP INDEX IF EXISTS idx_disbursements_status;
+DROP INDEX IF EXISTS idx_receiver_wallets_status;
+
+-- circle_transfer_requests
 ALTER TABLE circle_transfer_requests
     DROP CONSTRAINT circle_transfer_or_payout,
     DROP COLUMN circle_payout_id CASCADE;
 
+-- circle_recipients
 DROP TRIGGER refresh_circle_recipient_updated_at ON circle_recipients;
 DROP TABLE circle_recipients CASCADE;
 DROP TYPE circle_recipient_status;
