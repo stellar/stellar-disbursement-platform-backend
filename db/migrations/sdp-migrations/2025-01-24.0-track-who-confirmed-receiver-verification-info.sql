eba0a8f7789cd4f47809f42f9a48d485af2b6a73
Marcelo Salloum <marcelo@stellar.org>
2025-01-24 15:03:21 -0800

    Fix migration

--- db/migrations/sdp-migrations/2025-01-24.0-track-who-confirmed-receiver-verification-info.sql
+++ db/migrations/sdp-migrations/2025-01-24.0-track-who-confirmed-receiver-verification-info.sql
@@ -4,10 +4,16 @@
 CREATE TYPE confirmed_by_type AS ENUM ('RECEIVER', 'USER');
 
 ALTER TABLE receiver_verifications
-    ADD COLUMN confirmed_by_type confirmed_by_type NOT NULL DEFAULT 'RECEIVER',
+    ADD COLUMN confirmed_by_type confirmed_by_type,
     ADD COLUMN confirmed_by_id VARCHAR(36);
 
-UPDATE receiver_verifications SET confirmed_by_id = receiver_id WHERE confirmed_at IS NOT NULL;
+UPDATE
+    receiver_verifications
+SET
+    confirmed_by_id = receiver_id,
+    confirmed_by_type = 'RECEIVER'
+WHERE
+    confirmed_at IS NOT NULL;
 
 -- Add constraint where confirmed_by_id cannot be empty if confirmed_at is populated
 ALTER TABLE receiver_verifications
@@ -17,7 +23,7 @@ ALTER TABLE receiver_verifications
 -- +migrate Down
 ALTER TABLE receiver_verifications
     DROP CONSTRAINT confirmed_by_id_not_empty_when_completed,
-    DROP COLUMN confirmed_by,
+    DROP COLUMN confirmed_by_type,
     DROP COLUMN confirmed_by_id;
 
 DROP TYPE confirmed_by_type;
