2ee6d3c40406b29d5ad0135b1ec8c6790d72956d
Marcelo Salloum <marcelo@stellar.org>
2025-02-10 22:19:39 -0800

    Split memo-related tests for better organization

--- internal/services/paymentdispatchers/circle_payment_transfer_dispatcher_test.go
+++ internal/services/paymentdispatchers/circle_payment_transfer_dispatcher_test.go
@@ -20,11 +20,141 @@ import (
 	"github.com/stellar/stellar-disbursement-platform-backend/stellar-multitenant/pkg/tenant"
 )
 
-func Test_CirclePaymentTransferDispatcher_DispatchPayments(t *testing.T) {
+func Test_CirclePaymentTransferDispatcher_DispatchPayments_failure(t *testing.T) {
 	dbt := dbtest.Open(t)
 	defer dbt.Close()
-	dbConnectionPool, err := db.OpenDBConnectionPool(dbt.DSN)
-	require.NoError(t, err)
+	dbConnectionPool, outerErr := db.OpenDBConnectionPool(dbt.DSN)
+	require.NoError(t, outerErr)
+	defer dbConnectionPool.Close()
+
+	tenantID := "tenant-id"
+	ctx := context.Background()
+	models, outerErr := data.NewModels(dbConnectionPool)
+	require.NoError(t, outerErr)
+
+	circleWalletID := "22322112"
+
+	// Disbursement
+	disbursement := data.CreateDisbursementFixture(t, ctx, dbConnectionPool, models.Disbursements, &data.Disbursement{})
+	// Receiver
+	receiver := data.CreateReceiverFixture(t, ctx, dbConnectionPool, &data.Receiver{})
+	// Receiver Wallets
+	receiverWallet := data.CreateReceiverWalletFixture(t, ctx, dbConnectionPool, receiver.ID, disbursement.Wallet.ID, data.RegisteredReceiversWalletStatus)
+	payment := data.CreatePaymentFixture(t, ctx, dbConnectionPool, models.Payment, &data.Payment{
+		ReceiverWallet: receiverWallet,
+		Disbursement:   disbursement,
+		Asset:          *disbursement.Asset,
+		Amount:         "100",
+		Status:         data.ReadyPaymentStatus,
+	})
+
+	testCases := []struct {
+		name              string
+		paymentToDispatch *data.Payment
+		wantErr           error
+		fnSetup           func(*testing.T, *circle.MockService)
+		fnAssert          func(*testing.T, data.Payment)
+	}{
+		{
+			name:              "failure validating payment ready for sending",
+			paymentToDispatch: &data.Payment{ID: "123"},
+			wantErr:           fmt.Errorf("payment with ID 123 does not exist"),
+		},
+		{
+			name:              "payment marked as failed when posting circle transfer fails",
+			paymentToDispatch: payment,
+			wantErr:           nil,
+			fnSetup: func(t *testing.T, m *circle.MockService) {
+				transferRequest, setupErr := models.CircleTransferRequests.Insert(ctx, payment.ID)
+				require.NoError(t, setupErr)
+
+				m.On("SendTransfer", ctx, circle.PaymentRequest{
+					APIType:                   circle.APITypeTransfers,
+					SourceWalletID:            circleWalletID,
+					DestinationStellarAddress: payment.ReceiverWallet.StellarAddress,
+					DestinationStellarMemo:    payment.ReceiverWallet.StellarMemo,
+					Amount:                    payment.Amount,
+					StellarAssetCode:          payment.Asset.Code,
+					IdempotencyKey:            transferRequest.IdempotencyKey,
+				}).
+					Return(nil, fmt.Errorf("posting transfer to Circle")).
+					Once()
+			},
+			fnAssert: func(t *testing.T, p data.Payment) {
+				assert.Equal(t, data.FailedPaymentStatus, p.Status)
+			},
+		},
+		{
+			name:              "error updating circle transfer request",
+			paymentToDispatch: payment,
+			wantErr:           fmt.Errorf("updating circle transfer request: transfer cannot be nil"),
+			fnSetup: func(t *testing.T, m *circle.MockService) {
+				m.On("SendTransfer", ctx, mock.AnythingOfType("circle.PaymentRequest")).
+					Return(nil, nil).
+					Once()
+			},
+		},
+		{
+			name:              "error updating payment status for completed request",
+			paymentToDispatch: payment,
+			wantErr:           fmt.Errorf("invalid input value for enum circle_transfer_status"),
+			fnSetup: func(t *testing.T, m *circle.MockService) {
+				m.On("SendTransfer", ctx, mock.AnythingOfType("circle.PaymentRequest")).
+					Return(&circle.Transfer{
+						ID:     "transfer_id",
+						Status: "wrong-status",
+					}, nil).
+					Once()
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			dbTx, err := dbConnectionPool.BeginTxx(ctx, nil)
+			require.NoError(t, err)
+			defer func() {
+				err = dbTx.Rollback()
+				require.NoError(t, err)
+			}()
+
+			mCircleService := circle.NewMockService(t)
+			mDistAccountResolver := &mocks.MockDistributionAccountResolver{}
+			mDistAccountResolver.
+				On("DistributionAccountFromContext", ctx).
+				Return(schema.TransactionAccount{
+					Type:           schema.DistributionAccountCircleDBVault,
+					CircleWalletID: circleWalletID,
+					Status:         schema.AccountStatusActive,
+				}, nil).Maybe()
+
+			dispatcher := NewCirclePaymentTransferDispatcher(models, mCircleService, mDistAccountResolver)
+
+			if tc.fnSetup != nil {
+				tc.fnSetup(t, mCircleService)
+			}
+
+			err = dispatcher.DispatchPayments(ctx, dbTx, tenantID, []*data.Payment{tc.paymentToDispatch})
+			if tc.wantErr != nil {
+				assert.ErrorContains(t, err, tc.wantErr.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+
+			if tc.fnAssert != nil {
+				paymentFromDB, err := models.Payment.Get(ctx, tc.paymentToDispatch.ID, dbTx)
+				require.NoError(t, err)
+				tc.fnAssert(t, *paymentFromDB)
+			}
+		})
+	}
+}
+
+func Test_CirclePaymentTransferDispatcher_DispatchPayments_success(t *testing.T) {
+	dbt := dbtest.Open(t)
+	defer dbt.Close()
+	dbConnectionPool, outerErr := db.OpenDBConnectionPool(dbt.DSN)
+	require.NoError(t, outerErr)
 	defer dbConnectionPool.Close()
 
 	tenantID := "tenant-id"
@@ -35,8 +165,8 @@ func Test_CirclePaymentTransferDispatcher_DispatchPayments(t *testing.T) {
 
 	ctx := context.Background()
 	ctx = tenant.SaveTenantInContext(ctx, &tnt)
-	models, err := data.NewModels(dbConnectionPool)
-	require.NoError(t, err)
+	models, outerErr := data.NewModels(dbConnectionPool)
+	require.NoError(t, outerErr)
 
 	circleWalletID := "22322112"
 	circleTransferID := uuid.NewString()
@@ -60,11 +190,11 @@ func Test_CirclePaymentTransferDispatcher_DispatchPayments(t *testing.T) {
 	})
 
 	rwWithoutMemo := data.CreateReceiverWalletFixture(t, ctx, dbConnectionPool, receiver.ID, disbursementB.Wallet.ID, data.RegisteredReceiversWalletStatus)
-	err = models.ReceiverWallet.Update(ctx, rwWithoutMemo.ID, data.ReceiverWalletUpdate{
+	outerErr = models.ReceiverWallet.Update(ctx, rwWithoutMemo.ID, data.ReceiverWalletUpdate{
 		StellarMemo:     utils.Ptr(""),
 		StellarMemoType: utils.Ptr(schema.MemoType("")),
 	}, dbConnectionPool)
-	require.NoError(t, err)
+	require.NoError(t, outerErr)
 	paymentWithoutMemo := data.CreatePaymentFixture(t, ctx, dbConnectionPool, models.Payment, &data.Payment{
 		ReceiverWallet: rwWithoutMemo,
 		Disbursement:   disbursementB,
@@ -73,219 +203,119 @@ func Test_CirclePaymentTransferDispatcher_DispatchPayments(t *testing.T) {
 		Status:         data.ReadyPaymentStatus,
 	})
 
-	t.Run("🔴", func(t *testing.T) {
-		failureTestCases := []struct {
-			name               string
-			paymentsToDispatch []*data.Payment
-			wantErr            error
-			fnSetup            func(*testing.T, *circle.MockService)
-		}{
-			{
-				name: "failure validating payment ready for sending",
-				paymentsToDispatch: []*data.Payment{
-					{ID: "123"},
-				},
-				wantErr: fmt.Errorf("payment with ID 123 does not exist"),
+	testCases := []struct {
+		name                string
+		IsTenantMemoEnabled bool
+		paymentToDispatch   *data.Payment
+		fnAssertMemo        func(t *testing.T, p data.Payment, pReq circle.PaymentRequest)
+	}{
+		{
+			name:                "posting a Circle Transfer with ReceiverWallet memo",
+			IsTenantMemoEnabled: false,
+			paymentToDispatch:   paymentWithMemo,
+			fnAssertMemo: func(t *testing.T, p data.Payment, pReq circle.PaymentRequest) {
+				assert.Equal(t, p.ReceiverWallet.StellarMemo, pReq.DestinationStellarMemo)
 			},
-			{
-				name:               "payment marked as failed when posting circle transfer fails",
-				paymentsToDispatch: []*data.Payment{paymentWithMemo},
-				wantErr:            nil,
-				fnSetup: func(t *testing.T, m *circle.MockService) {
-					transferRequest, setupErr := models.CircleTransferRequests.Insert(ctx, paymentWithMemo.ID)
-					require.NoError(t, setupErr)
-
-					m.On("SendTransfer", ctx, circle.PaymentRequest{
-						APIType:                   circle.APITypeTransfers,
-						SourceWalletID:            circleWalletID,
-						DestinationStellarAddress: paymentWithMemo.ReceiverWallet.StellarAddress,
-						DestinationStellarMemo:    paymentWithMemo.ReceiverWallet.StellarMemo,
-						Amount:                    paymentWithMemo.Amount,
-						StellarAssetCode:          paymentWithMemo.Asset.Code,
-						IdempotencyKey:            transferRequest.IdempotencyKey,
-					}).
-						Return(nil, fmt.Errorf("error posting transfer to Circle")).
-						Once()
-				},
+		},
+		{
+			name:                "posting a Circle Transfer without ReceiverWallet nor Organization memo",
+			IsTenantMemoEnabled: false,
+			paymentToDispatch:   paymentWithoutMemo,
+			fnAssertMemo: func(t *testing.T, p data.Payment, pReq circle.PaymentRequest) {
+				assert.Empty(t, pReq.DestinationStellarMemo)
 			},
-			{
-				name:               "error updating circle transfer request",
-				paymentsToDispatch: []*data.Payment{paymentWithMemo},
-				wantErr:            fmt.Errorf("updating circle transfer request: transfer cannot be nil"),
-				fnSetup: func(t *testing.T, m *circle.MockService) {
-					m.On("SendTransfer", ctx, mock.AnythingOfType("circle.PaymentRequest")).
-						Return(nil, nil).
-						Once()
-				},
+		},
+		{
+			name:                "posting a Circle Transfer with Organization memo enabled",
+			IsTenantMemoEnabled: true,
+			paymentToDispatch:   paymentWithoutMemo,
+			fnAssertMemo: func(t *testing.T, p data.Payment, pReq circle.PaymentRequest) {
+				assert.Equal(t, GenerateHashFromBaseURL(*tnt.BaseURL), pReq.DestinationStellarMemo)
 			},
-			{
-				name:               "error updating payment status for completed request",
-				paymentsToDispatch: []*data.Payment{paymentWithMemo},
-				wantErr:            fmt.Errorf("invalid input value for enum circle_transfer_status"),
-				fnSetup: func(t *testing.T, m *circle.MockService) {
-					m.On("SendTransfer", ctx, mock.AnythingOfType("circle.PaymentRequest")).
-						Return(&circle.Transfer{
-							ID:     "transfer_id",
-							Status: "wrong-status",
-						}, nil).
-						Once()
-				},
-			},
-		}
+		},
+	}
 
-		for _, tc := range failureTestCases {
-			t.Run(tc.name, func(t *testing.T) {
-				dbtx, runErr := dbConnectionPool.BeginTxx(ctx, nil)
-				require.NoError(t, runErr)
-				defer func() {
-					err = dbtx.Rollback()
-					require.NoError(t, err)
-				}()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := models.Organizations.Update(ctx, &data.OrganizationUpdate{IsTenantMemoEnabled: utils.Ptr(tc.IsTenantMemoEnabled)})
+			require.NoError(t, err)
 
-				mCircleService := circle.NewMockService(t)
-				mDistAccountResolver := &mocks.MockDistributionAccountResolver{}
-				mDistAccountResolver.
-					On("DistributionAccountFromContext", ctx).
-					Return(schema.TransactionAccount{
-						Type:           schema.DistributionAccountCircleDBVault,
-						CircleWalletID: circleWalletID,
-						Status:         schema.AccountStatusActive,
-					}, nil).Maybe()
+			dbTx, err := dbConnectionPool.BeginTxx(ctx, nil)
+			require.NoError(t, err)
+			defer func() {
+				require.NoError(t, dbTx.Rollback())
+				data.DeleteAllCircleTransferRequests(t, ctx, dbConnectionPool)
+			}()
 
-				dispatcher := NewCirclePaymentTransferDispatcher(models, mCircleService, mDistAccountResolver)
+			mCircleService := circle.NewMockService(t)
+			mDistAccountResolver := &mocks.MockDistributionAccountResolver{}
+			mDistAccountResolver.
+				On("DistributionAccountFromContext", ctx).
+				Return(schema.TransactionAccount{
+					Type:           schema.DistributionAccountCircleDBVault,
+					CircleWalletID: circleWalletID,
+					Status:         schema.AccountStatusActive,
+				}, nil).Maybe()
 
-				if tc.fnSetup != nil {
-					tc.fnSetup(t, mCircleService)
-				}
+			dispatcher := NewCirclePaymentTransferDispatcher(models, mCircleService, mDistAccountResolver)
 
-				runErr = dispatcher.DispatchPayments(ctx, dbtx, tenantID, tc.paymentsToDispatch)
-				if tc.wantErr != nil {
-					assert.ErrorContains(t, runErr, tc.wantErr.Error())
-				} else {
-					assert.NoError(t, runErr)
-				}
-			})
-		}
-	})
+			p := tc.paymentToDispatch
+			mCircleService.
+				On("SendTransfer", ctx, mock.AnythingOfType("circle.PaymentRequest")).
+				Run(func(args mock.Arguments) {
+					pReq, ok := args.Get(1).(circle.PaymentRequest)
+					require.True(t, ok)
 
-	t.Run("🟢", func(t *testing.T) {
-		successfulTestCases := []struct {
-			name                string
-			IsTenantMemoEnabled bool
-			paymentToDispatch   *data.Payment
-			fnAssertMemo        func(t *testing.T, p data.Payment, pReq circle.PaymentRequest)
-		}{
-			{
-				name:                "success posting transfer to Circle Transfers with ReceiverWallet memo",
-				IsTenantMemoEnabled: false,
-				paymentToDispatch:   paymentWithMemo,
-				fnAssertMemo: func(t *testing.T, p data.Payment, pReq circle.PaymentRequest) {
-					assert.Equal(t, p.ReceiverWallet.StellarMemo, pReq.DestinationStellarMemo)
-				},
-			},
-			{
-				name:                "success posting transfer to Circle Transfers without ReceiverWallet nor Organization memo",
-				IsTenantMemoEnabled: false,
-				paymentToDispatch:   paymentWithoutMemo,
-				fnAssertMemo: func(t *testing.T, p data.Payment, pReq circle.PaymentRequest) {
-					assert.Empty(t, pReq.DestinationStellarMemo)
-				},
-			},
-			{
-				name:                "success posting transfer to Circle Transfers with Organization memo enabled",
-				IsTenantMemoEnabled: true,
-				paymentToDispatch:   paymentWithoutMemo,
-				fnAssertMemo: func(t *testing.T, p data.Payment, pReq circle.PaymentRequest) {
-					assert.Equal(t, GenerateHashFromBaseURL(*tnt.BaseURL), pReq.DestinationStellarMemo)
-				},
-			},
-		}
+					assert.Equal(t, circle.APITypeTransfers, pReq.APIType)
+					assert.Equal(t, circleWalletID, pReq.SourceWalletID)
+					assert.Equal(t, p.Amount, pReq.Amount)
+					assert.Equal(t, p.Asset.Code, pReq.StellarAssetCode)
+					assert.Equal(t, p.ReceiverWallet.StellarAddress, pReq.DestinationStellarAddress)
 
-		for _, tc := range successfulTestCases {
-			t.Run(tc.name, func(t *testing.T) {
-				err = models.Organizations.Update(ctx, &data.OrganizationUpdate{IsTenantMemoEnabled: utils.Ptr(tc.IsTenantMemoEnabled)})
-				require.NoError(t, err)
+					tc.fnAssertMemo(t, *p, pReq)
+				}).
+				Return(&circle.Transfer{
+					ID:     circleTransferID,
+					Status: circle.TransferStatusPending,
+					Source: circle.TransferAccount{
+						ID:   circleWalletID,
+						Type: circle.TransferAccountTypeWallet,
+					},
+					Destination: circle.TransferAccount{
+						Address:    p.ReceiverWallet.StellarAddress,
+						AddressTag: p.ReceiverWallet.StellarMemo,
+						Chain:      circle.StellarChainCode,
+					},
+					Amount: circle.Balance{
+						Amount:   p.Amount,
+						Currency: "USD",
+					},
+				}, nil).
+				Once()
 
-				dbtx, runErr := dbConnectionPool.BeginTxx(ctx, nil)
-				require.NoError(t, runErr)
-				defer func() {
-					err = dbtx.Rollback()
-					require.NoError(t, err)
+			err = dispatcher.DispatchPayments(ctx, dbTx, tenantID, []*data.Payment{p})
+			assert.NoError(t, err)
 
-					_, err = dbConnectionPool.ExecContext(ctx, "DELETE FROM circle_transfer_requests")
-					require.NoError(t, err)
-				}()
+			// Payment should be marked as pending
+			paymentFromDB, err := models.Payment.Get(ctx, p.ID, dbTx)
+			require.NoError(t, err)
+			assert.Equal(t, data.PendingPaymentStatus, paymentFromDB.Status)
 
-				mCircleService := circle.NewMockService(t)
-				mDistAccountResolver := &mocks.MockDistributionAccountResolver{}
-				mDistAccountResolver.
-					On("DistributionAccountFromContext", ctx).
-					Return(schema.TransactionAccount{
-						Type:           schema.DistributionAccountCircleDBVault,
-						CircleWalletID: circleWalletID,
-						Status:         schema.AccountStatusActive,
-					}, nil).Maybe()
+			// Transfer request is still not updated for the main connection pool
+			queryParams := data.QueryParams{Filters: map[data.FilterKey]interface{}{data.FilterKeyPaymentID: p.ID}}
+			transferRequest, err := models.CircleTransferRequests.Get(ctx, dbConnectionPool, queryParams)
+			require.NoError(t, err)
+			assert.Nil(t, transferRequest.CircleTransferID)
+			assert.Nil(t, transferRequest.SourceWalletID)
 
-				dispatcher := NewCirclePaymentTransferDispatcher(models, mCircleService, mDistAccountResolver)
-
-				p := tc.paymentToDispatch
-				mCircleService.
-					On("SendTransfer", ctx, mock.AnythingOfType("circle.PaymentRequest")).
-					Run(func(args mock.Arguments) {
-						pReq, ok := args.Get(1).(circle.PaymentRequest)
-						require.True(t, ok)
-
-						assert.Equal(t, circle.APITypeTransfers, pReq.APIType)
-						assert.Equal(t, circleWalletID, pReq.SourceWalletID)
-						assert.Equal(t, p.Amount, pReq.Amount)
-						assert.Equal(t, p.Asset.Code, pReq.StellarAssetCode)
-						assert.Equal(t, p.ReceiverWallet.StellarAddress, pReq.DestinationStellarAddress)
-
-						tc.fnAssertMemo(t, *p, pReq)
-					}).
-					Return(&circle.Transfer{
-						ID:     circleTransferID,
-						Status: circle.TransferStatusPending,
-						Source: circle.TransferAccount{
-							ID:   circleWalletID,
-							Type: circle.TransferAccountTypeWallet,
-						},
-						Destination: circle.TransferAccount{
-							Address:    p.ReceiverWallet.StellarAddress,
-							AddressTag: p.ReceiverWallet.StellarMemo,
-							Chain:      circle.StellarChainCode,
-						},
-						Amount: circle.Balance{
-							Amount:   p.Amount,
-							Currency: "USD",
-						},
-					}, nil).
-					Once()
-
-				runErr = dispatcher.DispatchPayments(ctx, dbtx, tenantID, []*data.Payment{p})
-				assert.NoError(t, runErr)
-
-				// Payment should be marked as pending
-				paymentFromDB, assertErr := models.Payment.Get(ctx, p.ID, dbtx)
-				require.NoError(t, assertErr)
-				assert.Equal(t, data.PendingPaymentStatus, paymentFromDB.Status)
-
-				// Transfer request is still not updated for the main connection pool
-				var transferRequest data.CircleTransferRequest
-				assertErr = dbConnectionPool.GetContext(ctx, &transferRequest, "SELECT * FROM circle_transfer_requests WHERE payment_id = $1", p.ID)
-				require.NoError(t, assertErr)
-				assert.Nil(t, transferRequest.CircleTransferID)
-				assert.Nil(t, transferRequest.SourceWalletID)
-
-				// Transfer request is updated for the transaction
-				assertErr = dbtx.GetContext(ctx, &transferRequest, "SELECT * FROM circle_transfer_requests WHERE payment_id = $1", p.ID)
-				require.NoError(t, assertErr)
-				assert.Equal(t, circleTransferID, *transferRequest.CircleTransferID)
-				assert.Equal(t, circleWalletID, *transferRequest.SourceWalletID)
-				assert.Equal(t, data.CircleTransferStatusPending, *transferRequest.Status)
-			})
-		}
-	})
+			// Transfer request is updated for the transaction
+			transferRequest, err = models.CircleTransferRequests.Get(ctx, dbTx, queryParams)
+			require.NoError(t, err)
+			assert.Equal(t, circleTransferID, *transferRequest.CircleTransferID)
+			assert.Equal(t, circleWalletID, *transferRequest.SourceWalletID)
+			assert.Equal(t, data.CircleTransferStatusPending, *transferRequest.Status)
+		})
+	}
 }
 
 func Test_CirclePaymentTransferDispatcher_SupportedPlatform(t *testing.T) {
