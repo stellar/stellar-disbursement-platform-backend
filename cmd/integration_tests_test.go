82a5b3659325dcf97d268fcbd7c98745cb8c1b28
Marcelo Salloum <marcelo@stellar.org>
2025-03-21 16:26:49 -0300

    Create a helper to test that a certain command exists with `os.Exit(1)`.

--- cmd/integration_tests_test.go
+++ cmd/integration_tests_test.go
@@ -3,16 +3,13 @@ package cmd
 import (
 	"context"
 	"errors"
-	"fmt"
-	"os"
-	"os/exec"
 	"testing"
 
 	"github.com/spf13/cobra"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
+	"github.com/stellar/stellar-disbursement-platform-backend/cmd/utils"
 	"github.com/stellar/stellar-disbursement-platform-backend/internal/integrationtests"
 )
 
@@ -81,29 +78,12 @@ func Test_IntegrationTestsCommand_StartIntegrationTestsCommand(t *testing.T) {
 	})
 
 	t.Run("exit with status 1 when IntegrationTestsService fails", func(t *testing.T) {
-		if os.Getenv("TEST_FATAL") == "1" {
+		utils.AssertFuncExitsWithFatal(t, func() {
 			serviceMock.
 				On("StartIntegrationServe", context.Background(), *integrationTestsOpts).
 				Return(errors.New("unexpected error"))
-
-			err := parentCmdMock.Execute()
-			require.NoError(t, err)
-
-			return
-		}
-
-		// We're using a strategy to setup a cmd inside the test that calls the test itself and verifies if it exited with exit status '1'.
-		// Ref: https://go.dev/talks/2014/testing.slide#23
-		cmd := exec.Command(os.Args[0], fmt.Sprintf("-test.run=%s", t.Name()))
-		cmd.Env = append(os.Environ(), "TEST_FATAL=1")
-
-		err := cmd.Run()
-		if exitError, ok := err.(*exec.ExitError); ok {
-			assert.False(t, exitError.Success())
-			return
-		}
-
-		t.Fatalf("process ran with err %v, want exit status 1", err)
+			_ = parentCmdMock.Execute()
+		}, "Error starting integration tests: unexpected error")
 	})
 
 	t.Run("executes the start integration tests command successfully", func(t *testing.T) {
@@ -152,29 +132,12 @@ func Test_IntegrationTestsCommand_CreateIntegrationTestsDataCommand(t *testing.T
 	})
 
 	t.Run("exit with status 1 when IntegrationTestsService fails", func(t *testing.T) {
-		if os.Getenv("TEST_FATAL") == "1" {
+		utils.AssertFuncExitsWithFatal(t, func() {
 			serviceMock.
 				On("CreateTestData", context.Background(), *integrationTestsOpts).
 				Return(errors.New("unexpected error"))
-
-			err := parentCmdMock.Execute()
-			require.NoError(t, err)
-
-			return
-		}
-
-		// We're using a strategy to setup a cmd inside the test that calls the test itself and verifies if it exited with exit status '1'.
-		// Ref: https://go.dev/talks/2014/testing.slide#23
-		cmd := exec.Command(os.Args[0], fmt.Sprintf("-test.run=%s", t.Name()))
-		cmd.Env = append(os.Environ(), "TEST_FATAL=1")
-
-		err := cmd.Run()
-		if exitError, ok := err.(*exec.ExitError); ok {
-			assert.False(t, exitError.Success())
-			return
-		}
-
-		t.Fatalf("process ran with err %v, want exit status 1", err)
+			_ = parentCmdMock.Execute()
+		}, "Error creating integration tests data: unexpected error")
 	})
 
 	t.Run("executes the create integration tests data command successfully", func(t *testing.T) {
--- cmd/utils/test_helpers.go
+++ cmd/utils/test_helpers.go
@@ -1,9 +1,14 @@
 package utils
 
 import (
+	"bytes"
+	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 // clearTestEnvironment removes all envs from the test environment. It's useful
@@ -14,4 +19,32 @@ func ClearTestEnvironment(t *testing.T) {
 		key := env[:strings.Index(env, "=")]
 		t.Setenv(key, "")
 	}
+}
+
+// AssertFuncExitsWithFatal asserts that a function exits with a fatal error, usually `os.Exit(1)`.
+func AssertFuncExitsWithFatal(t *testing.T, fatalFunc func(), stdErrContains ...string) {
+	t.Helper()
+
+	if os.Getenv("TEST_FATAL") == "1" {
+		// Run the fatal function that will call os.Exit(n)
+		fatalFunc()
+		return
+	}
+
+	cmd := exec.Command(os.Args[0], fmt.Sprintf("-test.run=%s", t.Name()))
+	cmd.Env = append(os.Environ(), "TEST_FATAL=1")
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if exitError, ok := err.(*exec.ExitError); ok {
+		require.False(t, exitError.Success())
+		return
+	}
+
+	for _, stdErrContain := range stdErrContains {
+		require.Contains(t, stderr.String(), stdErrContain)
+	}
+
+	t.Fatalf("process ran with err %v, want exit status 1", err)
 }
