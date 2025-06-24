82c841e50d7a52d8fc5342b68a86368892fd68d4
Denys Pohrebniak <dpohr@softserveinc.com>
2025-06-24 21:31:55 +0300

    SDP-1645 post develop merge cleanup

--- internal/data/query_params.go
+++ internal/data/query_params.go
@@ -49,7 +49,7 @@ const (
 	FilterKeyCreatedAtAfter   FilterKey = "created_at_after"
 	FilterKeyCreatedAtBefore  FilterKey = "created_at_before"
 	FilterKeySyncAttempts     FilterKey = "sync_attempts"
-	FilterKeyPaymentType      FilterKey = "payment_type"
+	FilterKeyPaymentType      FilterKey = "type"
 )
 
 func (fk FilterKey) Equals() string {
