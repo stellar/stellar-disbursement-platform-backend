de07eebbd55b1607e176399465298fa00b57db82
Jamie Li <lijamie@gmail.com>
2025-11-13 16:00:41 -0800

    Fix manager_test.go failures and add unit test

--- stellar-multitenant/internal/provisioning/manager_test.go
+++ stellar-multitenant/internal/provisioning/manager_test.go
@@ -480,6 +480,7 @@ func getExpectedTablesAfterMigrationsApplied() []string {
 		"receivers",
 		"receivers_audit",
 		"sdp_migrations",
+		"sep24_transactions",
 		"short_urls",
 		"wallets",
 		"wallets_assets",
