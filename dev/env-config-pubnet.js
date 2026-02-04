92b16e21b9306e8f762864334713ebd8806ab04e
Philip Liu <12836897+philipliu@users.noreply.github.com>
2026-02-04 10:31:47 -0500

    Chore: Update helm chart with embedded wallet defaults (#1034)
    ### What
    
    This updates the helm chart and wizard with defaults for the embedded
    wallet configuration. Between testnet resets, the wallet Wasm needs to
    be reuploaded, and the SEP-45 contract needs to be redeployed. I've done
    so manually right now, but I will add a GH workflow to automate this.
    
    ### Why
    
    Make embedded wallets easier to set up.
    
    ### Known limitations
    
    I've only checked that the helm chart can generate a manifest, but I
    haven't tried deploying it locally on my machine. However, I tested the
    flow end-to-end in a multitenant setup with the wizard.
    
    ### Checklist
    
    - [x] Title follows `SDP-1234: Add new feature` or `Chore: Refactor
    package xyz` format. The Jira ticket code was included if available.
    - [x] PR has a focused scope and doesn't mix features with refactoring
    - [x] Tests are included (if applicable)
    - [ ] `CHANGELOG.md` is updated (if applicable)
    - [ ] CONFIG/SECRETS changes are updated in helmcharts and deployments
    (if applicable)
    - [ ] Preview deployment works as expected
    - [ ] Ready for production

--- dev/env-config-pubnet.js
+++ dev/env-config-pubnet.js
@@ -2,6 +2,7 @@ window._env_ = {
     API_URL: "http://localhost:8000",
     STELLAR_EXPERT_URL: "https://stellar.expert/explorer/public",
     HORIZON_URL: "https://horizon.stellar.org",
+    RPC_ENABLED: true,
     USDC_ASSET_ISSUER: "GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN",
     RECAPTCHA_SITE_KEY: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
     SINGLE_TENANT_MODE: true
