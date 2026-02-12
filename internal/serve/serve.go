f7efd7edfa4c8028772b1e78ce02f5034df897ae
Jiahui Hu <jiahuihu0425@gmail.com>
2026-02-12 17:10:24 -0500

    SDP-2009: Add endpoint for fetch captcha config (#1052)
    ### What
    
    1. Add `/app-config` endpoint to fetch captcha config for FE use
    2. Change`forgot_password_handler.go` to check `IsCAPTCHADisabled()`
    instead of env var `h.ReCAPTCHADisabled`, which is only the env-level
    flag.
    
    ### Why
    
    reCAPTCHA v3
    
    ### Known limitations
    
    To test reCAPTCHA v3 locally, you will need to update `.env` with
    ```
    CAPTCHA_TYPE="GOOGLE_RECAPTCHA_V3"
    RECAPTCHA_SITE_KEY="6LcgOWcsAAAAAJkeyQHEW4-yA-Eu79kYHDYXeznF"
    RECAPTCHA_SITE_SECRET_KEY="6LcgOWcsAAAAACGUHDBr7VpcZ_C5BUjRPuBk9EUP"
    ```
    and `env-config-testnet.js` (the nginx)with
    ```
    RECAPTCHA_SITE_KEY: "6LcgOWcsAAAAAJkeyQHEW4-yA-Eu79kYHDYXeznF",
    ```
    
    ### Checklist
    
    - [x] Title follows `SDP-1234: Add new feature` or `Chore: Refactor
    package xyz` format. The Jira ticket code was included if available.
    - [x] PR has a focused scope and doesn't mix features with refactoring
    - [x] Tests are included (if applicable)
    - [x] `CHANGELOG.md` is updated (if applicable)
    - [ ] If contracts changed, run the `Contract WASM Artifacts` workflow
    and open a PR to update the WASMs on `dev`
    - [ ] CONFIG/SECRETS changes are updated in helmcharts and deployments
    (if applicable)
    - [ ] Preview deployment works as expected
    - [ ] Ready for production

--- internal/serve/serve.go
+++ internal/serve/serve.go
@@ -693,6 +693,13 @@ func handleHTTP(o ServeOptions) *chi.Mux {
 	mux.Group(func(r chi.Router) {
 		r.Use(middleware.EnsureTenantMiddleware)
 
+		r.Get("/app-config", httphandler.AppConfigHandler{
+			Models:            o.Models,
+			CAPTCHAType:       o.CAPTCHAType,
+			ReCAPTCHASiteKey:  o.ReCAPTCHASiteKey,
+			ReCAPTCHADisabled: o.DisableReCAPTCHA,
+		}.ServeHTTP)
+
 		r.Get("/organization/logo", httphandler.OrganizationLogoHandler{
 			Models:        o.Models,
 			PublicFilesFS: publicfiles.PublicFiles,
