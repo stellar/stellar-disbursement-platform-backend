ec0dd1bffe59592d98331cf00394a36bbdc49045
Konstantinos Rokanas <konsta.rokanas@hypotenuse.ca>
2026-09-22 22:07:20 +0300

    Restore image decoding import warning comment

--- internal/utils/logo.go
+++ internal/utils/logo.go
@@ -7,7 +7,10 @@ import (
 	"image"
 	"strings"
 
-	// Register the PNG and JPEG decoders that image.DecodeConfig and image.Decode rely on.
+	// Don't remove the `image/jpeg` and `image/png` packages import unless
+	// the `image` package is no longer necessary.
+	// It registers the `Decoders` to handle the image decoding - `image.Decode`.
+	// See https://pkg.go.dev/image#pkg-overview
 	_ "image/jpeg"
 	_ "image/png"
 )
